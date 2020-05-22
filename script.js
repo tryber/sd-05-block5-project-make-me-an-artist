6ba8ec8d6b2cd6328e58d1a0395f7f77e3ecc08f
Marylange <marylangess@gmail.com>
2020-05-22 17:27:22 -0300

    refazendo projeto

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+function criarQuadros() {
+  const div1 = document.querySelector('.session1');
+
+  for(let i = 0; i < 5; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    div1.appendChild(newDiv);
+  }
+}
+
+function criarQuadros2() {
+  const div1 = document.querySelector('.session2');
+
+  for(let i = 0; i < 5; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    div1.appendChild(newDiv);
+  }
+}
+
+function criarQuadros3() {
+  const div1 = document.querySelector('.session3');
+
+  for(let i = 0; i < 5; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    div1.appendChild(newDiv);
+  }
+}
+
+function criarQuadros4() {
+  const div1 = document.querySelector('.session4');
+
+  for(let i = 0; i < 5; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    div1.appendChild(newDiv);
+  }
+}
+
+function criarQuadros5() {
+  const div1 = document.querySelector('.session5');
+
+  for(let i = 0; i < 5; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    div1.appendChild(newDiv);
+  }
+}
+
+criarQuadros();
+criarQuadros2();
+criarQuadros3();
+criarQuadros4();
+criarQuadros5();
+
+
 let corActual = 'black';
 function aplicaCorAtual(id) {
   let caixasPixelBoard = document.querySelector(`.${id}`);
