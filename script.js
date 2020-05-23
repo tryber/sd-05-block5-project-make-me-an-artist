660050223d59d70e3f124231e2898bc5a95f89bb
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-23 15:44:25 -0300

    Parte III

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 window.onload = function () {
+  buildPalette();
   function buildPalette() {
     const palette = document.getElementsByClassName('color');
-    let colors = ['black', 'red', 'green', 'blue'];
-    for(let i=0 ; i<palette.length ; i++){
+    const colors = ['black', 'red', 'green', 'blue'];
+    for (let i = 0 ; i < palette.length ; i++) {
       palette[i].style.backgroundColor = colors[i];
-      if(i == 0) palette[i].classList.add('selected');
+      if(i === 0) palette[i].classList.add('selected');
+    }
+    buildGrid();
+  }
+  function buildGrid() {
+    const cells = document.getElementsByClassName('pixel');
+    for (let i = 0 ; i < cells.length ; i++) {
+      cells[i].style.backgroundColor = 'white';
+      cells[i].addEventListener("click", function(el){
+        const selected = document.querySelector(".selected");
+        el.target.style.backgroundColor = selected.style.backgroundColor;
+      });
     }
   }
-  buildPalette();
 };
