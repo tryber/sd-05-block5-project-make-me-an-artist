d758e1348051c11c055096793834bcf2cfa14437
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-23 18:57:14 -0300

    Gerando o pixelboard e populando ele.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //gerando as cores da paleta
-
-let paleta = document.getElementById("color-palete");
+const paleta = document.getElementById("color-palette");
 const pixelPaleta = document.getElementsByClassName('color');
+const pixelBoard = document.getElementById("pixel-board");
 let selectedColor = "black";
+let nPixLine = 5;
 
 let colors = ["black", "blue", "green", "yellow"];
 
@@ -11,5 +12,38 @@ function setPaleteColors(colors) {
       pixelPaleta[pixel].style.backgroundColor = colors[pixel];
     }
 }
+
+// Gerando pixel-board
+
+function genBoardElement(className, idx) {
+  let boardElement = document.createElement("div");
+  boardElement.setAttribute("class", className);
+  boardElement.setAttribute("id", idx);
+  return boardElement; 
+}
+
+function genPixels(numberPix) {
+  let pixels = [];
+  for (let pixel = 0; pixel < numberPix; pixel+=1) {
+    let pix =  genBoardElement("pixel", `pixel-${pixel+1}`);
+    pixels.push(pix);
+  }
+  return pixels;
+}
+
+function populateBoard(pixels, elemento) {
+  for (let pixel of pixels) {
+    elemento.appendChild(pixel);
+  }
+}
+
+// Eventos
+window.addEventListener("load", function () {
+  let pixels = genPixels(nPixLine*nPixLine);
+  populateBoard(pixels, pixelBoard);
+  pixelBoard.style.width = `${nPixLine * 42}px`;
+  // pixelBoard.style.height = `${nPixLine * 41}px`;
+
+  });
 
 window.addEventListener("load", setPaleteColors(colors));
