42e871e2ef0a20ce91cf1fbb82b347770f7432f2
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-23 17:14:10 -0300

    Parte IV

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
+let palette, cells;
+function setCurrentColor(el) {
+  for (let p of palette) {
+    p.classList.remove('selected');
+  }
+  el.target.classList.add('selected');
+};
+function setPixelColor(el) {
+  const selected = document.querySelector('.selected');
+  el.target.style.backgroundColor = selected.style.backgroundColor;
+};
+function buildPalette() {
+  const colors = ['black', 'red', 'green', 'blue'];
+  for (let i = 0; i < palette.length; i += 1) {
+    palette[i].style.backgroundColor = colors[i];
+    if (i === 0) palette[i].classList.add('selected');
+    palette[i].addEventListener('click', setCurrentColor);
+  }
+};
+function buildGrid() {
+  for (let i = 0; i < cells.length; i += 1) {
+    cells[i].style.backgroundColor = 'white';
+    cells[i].addEventListener('click', setPixelColor);
+  }
+};
 window.onload = function () {
-  const palette = document.getElementsByClassName('color');
-  const cells = document.getElementsByClassName('pixel');
-  function buildPalette() {
-    const colors = ['black', 'red', 'green', 'blue'];
-    for (let i = 0; i < palette.length; i+=1) {
-      palette[i].style.backgroundColor = colors[i];
-      if (i === 0) palette[i].classList.add('selected');
-      palette[i].addEventListener('click', setCurrentColor);
-    }
-  };
-  function buildGrid() {
-    for (let i = 0; i < cells.length; i+=1) {
-      cells[i].style.backgroundColor = 'white';
-      cells[i].addEventListener('click', setPixelColor);
-    }
-  };
-  function setCurrentColor(el) {
-    for (let p of palette) {
-      p.classList.remove('selected');
-    }
-    el.target.classList.add('selected');
-  };
-  function setPixelColor(el) {
-    const selected = document.querySelector('.selected');
-    el.target.style.backgroundColor = selected.style.backgroundColor;
-  };
+  palette = document.getElementsByClassName('color');
+  cells = document.getElementsByClassName('pixel');
   buildPalette();
   buildGrid();
 };
