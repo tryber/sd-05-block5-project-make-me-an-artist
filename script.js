296f530bd4210946e70a154fd31a4d5966fe7e6a
pedrohcalado <pedrocalado22@gmail.com>
2020-05-23 13:23:17 -0300

    Add a event listener to change the color of the pixel when it's clicked

--- script.js
+++ script.js
@@ -1,44 +1,56 @@
-// create variable to store each color element
-const black = document.querySelector('#black');
-const yellow = document.querySelector('#yellow');
-const red = document.querySelector('#red');
-const blue = document.querySelector('#blue');
-
-// include class selected on black element when the window is loaded
 window.onload = function () {
-  black.className += ' selected';
-};
+  // create variable to store each color element
+  const black = document.querySelector('#black');
+  const yellow = document.querySelector('#yellow');
+  const red = document.querySelector('#red');
+  const blue = document.querySelector('#blue');
 
-// add a event listener to each collor in the palett
-black.addEventListener('click', function () {
-  if (black.className != 'color selected') {
-    black.className += ' selected';
-  }
-  yellow.className = 'color';
-  red.className = 'color';
-  blue.className = 'color';
-});
-yellow.addEventListener('click', function () {
-  if (yellow.className != 'color selected') {
-    yellow.className += ' selected';
-  }
-  black.className = 'color';
-  red.className = 'color';
-  blue.className = 'color';
-});
-red.addEventListener('click', function () {
-  if (red.className != 'color selected') {
-    red.className += ' selected';
-  }
-  yellow.className = 'color';
-  black.className = 'color';
-  blue.className = 'color';
-});
-blue.addEventListener('click', function () {
-  if (blue.className != 'color selected') {
-    blue.className += ' selected';
-  }
-  yellow.className = 'color';
-  red.className = 'color';
-  black.className = 'color';
-});
+  // include class selected on black element when the window  is loaded
+    black.className += ' selected';  
+    let selectedColor = document.querySelector('.color.selected').id;
+
+  // add a event listener to each collor in the palett
+  black.addEventListener('click', function () {
+    // if clas is not selected already, add class selected to   it
+    if (black.className != 'color selected') {
+      black.className += ' selected';
+    }
+    // keep other colors without the selected class
+    yellow.className = 'color';
+    red.className = 'color';
+    blue.className = 'color';
+    selectedColor = 'black';
+  });
+  yellow.addEventListener('click', function () {
+    if (yellow.className != 'color selected') {
+      yellow.className += ' selected';
+    }
+    black.className = 'color';
+    red.className = 'color';
+    blue.className = 'color';
+    selectedColor = 'yellow';
+  });
+  red.addEventListener('click', function () {
+    if (red.className != 'color selected') {
+      red.className += ' selected';
+    }
+    yellow.className = 'color';
+    black.className = 'color';
+    blue.className = 'color';
+    selectedColor = 'red';    
+  });
+  blue.addEventListener('click', function () {
+    if (blue.className != 'color selected') {
+      blue.className += ' selected';
+    }
+    yellow.className = 'color';
+    red.className = 'color';
+    black.className = 'color';
+    selectedColor = 'blue';
+  });
+
+  let board = document.getElementById('pixel-board');
+  board.addEventListener('click', function (event) {
+    event.target.id = selectedColor;
+  });
+}
