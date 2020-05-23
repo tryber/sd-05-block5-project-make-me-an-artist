c7134b72982574514257835a23d99d916134adba
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-23 17:24:59 -0300

    Definindo as cores iniciais e aplicando na paleta.

---
+++ script.js
@@ -0,0 +1,15 @@
+//gerando as cores da paleta
+
+let paleta = document.getElementById("color-palete");
+const pixelPaleta = document.getElementsByClassName('color');
+let selectedColor = "black";
+
+let colors = ["black", "blue", "green", "yellow"];
+
+function setPaleteColors() {
+  for (let pixel = 0; pixel < pixelPaleta.length; pixel+=1) {
+      pixelPaleta[pixel].style.backgroundColor = colors[pixel];
+    }
+}
+
+window.addEventListener("load", setPaleteColors);
