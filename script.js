b3471f0d937eee8245182c5d07a32644606f3572
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-23 16:52:35 -0300

    Parte IV

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 window.onload = function () {
-  buildPalette();
   function buildPalette() {
     const palette = document.getElementsByClassName('color');
+    
     const colors = ['black', 'red', 'green', 'blue'];
-    for (let i = 0 ; i < palette.length ; i++) {
+    for (let i = 0; i < palette.length; i++) {
       palette[i].style.backgroundColor = colors[i];
-      if(i === 0) palette[i].classList.add('selected');
+      if (i === 0) palette[i].classList.add('selected');
+      palette[i].addEventListener('click', function(el) {
+        for (p of palette) p.classList.remove("selected");
+        el.target.classList.add('selected');
+      });
     }
     buildGrid();
   }
   function buildGrid() {
     const cells = document.getElementsByClassName('pixel');
-    for (let i = 0 ; i < cells.length ; i++) {
+    for (let i = 0; i < cells.length; i++) {
       cells[i].style.backgroundColor = 'white';
-      cells[i].addEventListener("click", function(el){
-        const selected = document.querySelector(".selected");
+      cells[i].addEventListener('click', function(el) {
+        const selected = document.querySelector('.selected');
         el.target.style.backgroundColor = selected.style.backgroundColor;
       });
     }
   }
+  buildPalette();
 };
