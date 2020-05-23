914381e20460af1ddabc412e0b924af9018ad01b
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-05-23 19:23:02 -0300

    corrigindo coisas do Code Climate.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//gerando as cores da paleta
-const paleta = document.getElementById("color-palette");
+// Gerando as cores da paleta
+const paleta = document.getElementById('color-palette');
 const pixelPaleta = document.getElementsByClassName('color');
-const pixelBoard = document.getElementById("pixel-board");
-let selectedColor = "black";
+const pixelBoard = document.getElementById('pixel-board');
+let selectedColor = 'black';
 let nPixLine = 5;
 
-let colors = ["black", "blue", "green", "yellow"];
+let colors = ['black', 'blue', 'green', 'yellow'];
 
 function setPaleteColors(colors) {
-  for (let pixel = 0; pixel < pixelPaleta.length; pixel+=1) {
+  for (let pixel = 0; pixel < pixelPaleta.length; pixel += 1) {
       pixelPaleta[pixel].style.backgroundColor = colors[pixel];
     }
 }
@@ -16,16 +16,16 @@ function setPaleteColors(colors) {
 // Gerando pixel-board
 
 function genBoardElement(className, idx) {
-  let boardElement = document.createElement("div");
-  boardElement.setAttribute("class", className);
-  boardElement.setAttribute("id", idx);
-  return boardElement; 
+  const boardElement = document.createElement('div');
+  boardElement.setAttribute('class', className);
+  boardElement.setAttribute('id', idx);
+  return boardElement;
 }
 
 function genPixels(numberPix) {
   let pixels = [];
-  for (let pixel = 0; pixel < numberPix; pixel+=1) {
-    let pix =  genBoardElement("pixel", `pixel-${pixel+1}`);
+  for (let pixel = 0; pixel < numberPix; pixel += 1) {
+    let pix = genBoardElement('pixel', `pixel-${pixel + 1}`);
     pixels.push(pix);
   }
   return pixels;
@@ -38,12 +38,11 @@ function populateBoard(pixels, elemento) {
 }
 
 // Eventos
-window.addEventListener("load", function () {
-  let pixels = genPixels(nPixLine*nPixLine);
+window.addEventListener('load', function () {
+  let pixels = genPixels(nPixLine * nPixLine);
   populateBoard(pixels, pixelBoard);
   pixelBoard.style.width = `${nPixLine * 42}px`;
-  // pixelBoard.style.height = `${nPixLine * 41}px`;
+  }
+);
 
-  });
-
-window.addEventListener("load", setPaleteColors(colors));
+window.addEventListener('load', setPaleteColors(colors));
