f77ec75620d33c9ae2b7512f8b5823431ce81485
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-23 14:51:38 -0300

    Parte III

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-document.onload = function () {
-}
+window.onload = function () {
+
+  function buildGrid(width, height){
+
+    let board = document.getElementById("pixel-board");
+    for(let i=0 ; i<height ; i++) {
+      for(let j=0 ; j<width ; j++) {
+      let div = document.createElement("div");
+      let cel = board.appendChild(div);
+      cel.setAttribute("class", "pixel");
+      cel.style.left = (j*40).toString() + "px";
+      cel.style.top= (i*40).toString() + "px";
+      cel.addEventListener("click", function(){});
+      }
+    }
+  }
+
+  buildGrid(5, 5);
+};
