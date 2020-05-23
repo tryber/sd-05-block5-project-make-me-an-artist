b178ea50d59fac491fe1e65742378164c85ed440
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-05-23 17:24:09 -0300

    Parte V

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let palette, cells;
+let palette;
+let cells;
 function setCurrentColor(el) {
-  for (let p of palette) {
-    p.classList.remove('selected');
-  }
+  for(let i = 0; i < palette.length; i += 1) {
+    palette[i].classList.remove('selected');
+  };
   el.target.classList.add('selected');
-};
+}
 function setPixelColor(el) {
   const selected = document.querySelector('.selected');
   el.target.style.backgroundColor = selected.style.backgroundColor;
-};
+}
 function buildPalette() {
   const colors = ['black', 'red', 'green', 'blue'];
   for (let i = 0; i < palette.length; i += 1) {
@@ -16,13 +17,18 @@ function buildPalette() {
     if (i === 0) palette[i].classList.add('selected');
     palette[i].addEventListener('click', setCurrentColor);
   }
-};
+}
 function buildGrid() {
   for (let i = 0; i < cells.length; i += 1) {
     cells[i].style.backgroundColor = 'white';
     cells[i].addEventListener('click', setPixelColor);
   }
-};
+}
+function clearAll() {
+  for (let i = 0; i < cells.length; i += 1) {
+    cells[i].style.backgroundColor = 'white';
+  } 
+}
 window.onload = function () {
   palette = document.getElementsByClassName('color');
   cells = document.getElementsByClassName('pixel');
