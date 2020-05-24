557bd24d18d73db61953963cf083dfcd6cc77448
labelle <beelsdelonge@gmail.com>
2020-05-24 17:31:40 -0300

    ajustes codeclimate

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const botaoReset = document.getElementById('clear-board');
 const quadro = document.getElementById('pixel-board');
 
 botaoReset.addEventListener('click', function limparQuadro() {
-  quadro.style.backgroundColor = 'white' 
+  quadro.style.backgroundColor = 'white';
 });
