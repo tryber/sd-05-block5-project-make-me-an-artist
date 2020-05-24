cc15dd81947307c1b4ba070e06df0db49a6535e0
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-24 10:44:09 -0300

    adicionando Funções para montar o grid de pixels

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+let n = 5;
+let colorPalette = document.getElementById('color-palette');
+let pixelBoard = document.getElementById('pixel-board');
 
+function createBoardLines() {
+  let pixelLine = document.createElement('div');
+  pixelLine.className = 'pixel firstLine';
+  pixelBoard.appendChild(pixelLine);
+}
+
+function createBoardColumns() {
+  let pixelColumn = document.createElement('div');
+  pixelColumn.className = 'pixel';
+  pixelBoard.appendChild(pixelColumn);
+}
+
+function createGridOfPixel(n) {
+  pixelBoard.innerHTML = '';
+  for(let line = 1; line <= n; line += 1) {
+    createBoardLines();
+    for(let column = 2; column <= n; column += 1) {
+      createBoardColumns();
+    }
+  }
+}
+
+createGridOfPixel(n)
