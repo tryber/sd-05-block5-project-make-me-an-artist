86bf0a40a9b23b18d2f2c545fef380c3d17014c9
labelle <beelsdelonge@gmail.com>
2020-05-24 17:03:13 -0300

    ajustando JS do botao Limpar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let botao = document.getElementById('clear-board');
-let corInicial = document.getElementsByTagName('table').style.backgroundColor;
+let corInicial = document.getElementById('pixel-board').style.backgroundColor;
 
 botao.addEventListener('click', function limparQuadro() {
   corInicial = 'white';
