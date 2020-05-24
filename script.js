571b1f8f5d3f47bccce3f247accc2e178d2b845c
labelle <beelsdelonge@gmail.com>
2020-05-24 18:21:30 -0300

    color pallete add

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 const botaoReset = document.getElementById('clear-board');
-const quadro = document.getElementById('pixel-board');
+const quadrados = document.getElementsByClassName('pixel');
+const paletaDeCores = document.querySelectorAll('.color')
+
+
+let coresAtuais = ['preto', 'amarelo', 'azul', 'vermelho'];
+const cores = ['preto', 'amarelo', 'azul', 'vermelho'];
+let cor = "black"
+
+function selecionarCor() {
+    for (let i = 0; i < 4; i += 1) {
+        if (this.className === ` color ${coresAtuais[i]}`) {
+            removerSelected();
+            this.className = ` color ${coresAtuais[i]} selected`;
+            cor = cores[i];
+        }
+    }
+}
+
+for (let i = 0; i < paletaDeCores.length; i += 1) {
+    paletaDeCores[i].addEventListener('click', selecionarCor)
+}
+
+function atribuirCor() {
+    this.style.backgroungColor = "cor";
+}
+
+for (let i = 0; i < quadrados.length; i += 1) {
+    quadrados[i].addEventListener('click', atribuirCor)
+}
+function removerSelected() {
+    for (let i = 0; i < paletaDeCores.length; i += 1) {
+        paletaDeCores[i].className = ` color ${coresAtuais[i]}`
+    }
+}
 
 botaoReset.addEventListener('click', function limparQuadro() {
-  quadro.style.backgroundColor = 'white';
+    for (let i = 0; i < quadrados.length; i += 1) {
+        quadrados[i].className = 'pixel';
+        quadrados[i].style.background = 'white';
+    }
 });
