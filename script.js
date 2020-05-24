da6df4700f687aa217f85baf74e5cea69d17a462
preto115 <preto115@gmail.com>
2020-05-24 19:14:55 -0300

    implementando scripts  para adequar-se ao projeto_1

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 // Feito por Elisângelo
 // scripts para pintar os pixels de cores que foram seleciondas pelo usuario
 window.onload = function(){
-let selectedColor = document.querySelector(".color.black").className = "selected " + className ;
+let selectedColor = document.getElementsByClassName("black").className =selectedColor+" "+"selected";
 let clearButton = document.getElementById('clear-board');
 clearButton.addEventListener('click',limpaPixel);
-let pixelBoard = document.getElementsByClassName("pixel")[0];
+let pixelBoard = document.getElementsByClassName("pixel")[0].children;
 function limpaPixel(){
     for( limpa in pixelBoard){
-        [limpa].style.backgroundColor = "white";
-
+        [limpa].children.style.backgroundColor = "white";
     }
 }
-
-
 }
