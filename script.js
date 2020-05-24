84d9b7ba6a61be15bbf18feaff816c1227e9c290
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-05-24 11:31:58 -0300

    adicionado função para trocar a cor selecionada

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+// variables
 let n = 5;
 let colorPalette = document.getElementById('color-palette');
 let pixelBoard = document.getElementById('pixel-board');
+let target = '';
+let black = document.getElementById('black');
+let blue = document.getElementById('blue');
+let red = document.getElementById('red');
+let green = document.getElementById('green');
+let selected = document.getElementsByClassName('selected')[0];  
 
+// functions
 function createBoardLines() {
   let pixelLine = document.createElement('div');
   pixelLine.className = 'pixel firstLine';
@@ -23,5 +31,18 @@ function createGridOfPixel(n) {
     }
   }
 }
+
+function changeColor(){
+  target = event.target;
+  selected.classList.remove('selected');
+  target.classList.add('selected');
+  selected = document.getElementsByClassName('selected')[0];
+}
+
+// events
+black.addEventListener('click', changeColor);
+blue.addEventListener('click', changeColor);
+red.addEventListener('click', changeColor);
+green.addEventListener('click', changeColor);
 
 createGridOfPixel(n)
