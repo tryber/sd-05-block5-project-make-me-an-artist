c2bd60937b3cc4581a8b3e2777a6e1dda0620157
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-24 15:57:44 -0300

    começando a criar pixels vazios dentro do elemento pixel-board

---
+++ script.js
@@ -0,0 +1,16 @@
+const lineOfPixels = document.createElement('section');
+lineOfPixels.className = 'linha';
+
+const emptyPixel = document.createElement('div');
+emptyPixel.className = 'pixel';
+
+
+function createLineOfPixel() {
+  document.querySelector('#pixel-board').appendChild;
+}
+
+
+createLineOfPixel();
+createLineOfPixel();
+createLineOfPixel();
+createLineOfPixel();
