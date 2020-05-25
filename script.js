f2c7f7ff4eaafa4d01a1a91a3d63d94b7f5b593f
matambrosio <matmagamb1@gmail.com>
2020-05-25 15:17:12 -0300

    req dois tentativa 1

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-if(){
-  
+let pallete = document.querySelector("#color-pallete");
+pallete.addEventListener("mouseover",palleteColors)
+
+function palleteColors(eventoPallete){
+  console.log(eventoPallete.target);
 }
