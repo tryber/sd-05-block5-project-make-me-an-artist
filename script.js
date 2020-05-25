6767dd3356b357ab31ab602b986dcf04dd7eb40b
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-25 01:01:51 -0300

    add botao limpar

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const emptyPixel = document.createElement('div');
-emptyPixel.className = 'pixel';
-
-const lineOfPixels = document.createElement('section');
-lineOfPixels.className = 'line';
+function clearBoard() {
+  for (let i = 0; i < 5; i+=1) {
+    for (let n = 0; n < 5; n+=1) {
+      document.querySelectorAll('.linha')[i].chil.style.backgroundColor = 'white';
+    }
+  }
+}
