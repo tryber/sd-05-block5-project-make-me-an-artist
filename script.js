1b092473e9b23298f286683184f3ad23f26f76bb
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-25 15:42:33 -0300

    color palette added

---
+++ script.js
@@ -0,0 +1,9 @@
+let colorBase1 = document.querySelectorAll(".color")[0];
+let colorBase2 = document.querySelectorAll(".color")[1];
+let colorBase3 = document.querySelectorAll(".color")[2];
+let colorBase4 = document.querySelectorAll(".color")[3];
+
+colorBase1.style.background = "black";
+colorBase2.style.background = "green";
+colorBase3.style.background = "red";
+colorBase4.style.background = "pink";
