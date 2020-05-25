7557a76c7f7a36d3cbcc2e9ac6cc7cec63fc4838
preto115 <preto115@gmail.com>
2020-05-25 00:56:47 -0300

    implementando scripts  para adequar-se ao projeto_4

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function(){
 let selected = document.getElementsByClassName("black")[0];
 let cores = document.getElementById("color-palette");
 let clearButton = document.getElementById('clear-board');
-let pixelBoard = document.getElementById("pixel-board");
+let pixelBoard = document.querySelectorAll("pixel");
 
 let corSelecionada= '';
 cores.addEventListener("click", selecionaCor);
@@ -18,18 +18,20 @@ function selecionaCor(e){
     let selecao = document.querySelector(".selecao");
     let cor = e.target;
     selecao.innerHTML = cor.classList[1];
-    corSelecionada.classList.add(cor.classList[1]);
-    console.log(corSelecionada.className);
+    corSelecionada.classList= cor.classList[1];    
     console.log("passei por aqui");
 }
-function limpaPixel(){
-    for( limpa in pixelBoard){
-        pixelBoard[limpa].children.style = "background-color: white";
+function limpaPixel(e){
+    if(e.target){ 
+    for(let child = parent.firstChild; child != null; child = child.nextSibling ){
+        pixelBoard[child].style.backgroundColor = "white";
+        pixelBoard.classList[child] = corSelecionada.classList[0];
     }
+}
 }
 function colorePixel(e){
     let  pixelSelecionado = document.querySelectorAll("pixel")[e.target];
-    pixelSelecionado.style = corSelecionada.style.backgroundColor;  
+    pixelSelecionado.style = corSelecionada.style.backgroundColor  
 }
 
 }
