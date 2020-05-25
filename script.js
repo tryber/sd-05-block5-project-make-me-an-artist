aa313289c09276b7c8b753f620872bf0c16f30d3
Marylange <marylangess@gmail.com>
2020-05-25 12:39:27 -0300

    Revert "botão entre paleta e box"
    This reverts commit e695750ef10c0f115ced8103462dc259b510b57b.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//criar quadros dinâmicamente
 function criarQuadros() {
   const div1 = document.querySelector('.pixel-board');
 
@@ -9,40 +8,87 @@ function criarQuadros() {
   }
 }
 
-criarQuadros();
+function criarQuadros2() {
+  const div1 = document.querySelector('.session2');
+
+  for(let i = 0; i < 5; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    div1.appendChild(newDiv);
+  }
+}
+
+function criarQuadros3() {
+  const div1 = document.querySelector('.session3');
+
+  for(let i = 0; i < 5; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    div1.appendChild(newDiv);
+  }
+}
+
+function criarQuadros4() {
+  const div1 = document.querySelector('.session4');
+
+  for(let i = 0; i < 5; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    div1.appendChild(newDiv);
+  }
+}
+
+function criarQuadros5() {
+  const div1 = document.querySelector('.session5');
+
+  for(let i = 0; i < 5; i += 1) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'pixel';
+    div1.appendChild(newDiv);
+  }
+}
+
+function renderizarPixels() {
+  criarQuadros();
+  // criarQuadros2();
+  // criarQuadros3();
+  // criarQuadros4();
+  // criarQuadros5();
+}
+renderizarPixels();
 
 //evento de click nas cores
 const coresBlack = document.querySelector('.selected');
-coresBlack.addEventListener('click', corBlack);
+coresBlack.addEventListener('click', aplicarCorBlack);
 
 let corAtual = 'black';
 
-function corBlack() {
+function aplicarCorBlack() {
   corAtual = 'black';
 }
 
 const coresYellow = document.querySelector('.color-yellow');
-coresYellow.addEventListener('click', corYellow);
+coresYellow.addEventListener('click', aplicarCorYellow);
 
-function corYellow() {
+function aplicarCorYellow() {
   corAtual = 'yellow';
 }
 
 const coresBlue = document.querySelector('.color-blue');
-coresBlue.addEventListener('click', corBlue);
+coresBlue.addEventListener('click', aplicarCorBlue);
 
-function corBlue() {
+function aplicarCorBlue() {
   corAtual = 'blue';
 }
 
 const coresGreen = document.querySelector('.color-green');
-coresGreen.addEventListener('click', corGreen);
+coresGreen.addEventListener('click', aplicarCorGreen);
 
-function corGreen() {
+function aplicarCorGreen() {
   corAtual = 'green';
 }
 
-//Identificar click na caixa de pixel e aplicar cor
+//caixas de pixel em branco
 const pixels = document.querySelectorAll('.pixel');
 for(let i = 0; i < pixels.length; i += 1) {
   pixels[i].addEventListener('click', identicarPixel);
@@ -52,7 +98,7 @@ function identicarPixel(evento) {
   evento.target.style.backgroundColor = corAtual;
 }
 
-//limpar cores associando ao botão
+//limpar cores
 let botaoApagarCores = document.querySelector('#clear-board');
 botaoApagarCores.addEventListener('click', apagarEstilos);
 
