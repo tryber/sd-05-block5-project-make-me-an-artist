6032b739c5fd4fddc58716b220a8c8c482fbcd90
preto115 <preto115@gmail.com>
2020-05-24 23:36:24 -0300

    implementando scripts  para adequar-se ao projeto_2

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 // Feito por Elisângelo
 // scripts para pintar os pixels de cores que foram seleciondas pelo usuario
 window.onload = function(){
-let selectedColor = document.getElementsByClassName("black").className =selectedColor+" "+"selected";
+let selected = document.getElementsByClassName("black")[0];
+let cores = document.getElementById("color-palette");
 let clearButton = document.getElementById('clear-board');
+let pixelBoard = document.getElementById("pixel-board");
+
+let corSelecionada= '';
+cores.addEventListener("click", selecionaCor);
 clearButton.addEventListener('click',limpaPixel);
-let pixelBoard = document.getElementsByClassName("pixel")[0].children;
+pixelBoard.addEventListener("click", colorePixel);
+
+
+selected.classList.remove("selected");
+
+function selecionaCor(e){
+    let selecao = document.querySelector(".selecao");
+    let cor = e.target;
+    selecao.innerHTML = cor.classList[1];
+    corSelecionada.className = cor.classList[1].className;
+    console.log(corSelecionada.className);
+    console.log("passei por aqui");
+}
 function limpaPixel(){
     for( limpa in pixelBoard){
-        [limpa].children.style.backgroundColor = "white";
+        pixelBoard[limpa].children.style = "background-color: white";
     }
 }
+function colorePixel(e){
+    let  pixelSelecionado = document.querySelectorAll("pixel")[e.target];
+    pixelSelecionado.style = corSelecionada.style.backgroundColor;  
+}
+
 }
