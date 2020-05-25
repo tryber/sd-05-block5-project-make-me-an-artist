74877a1e6c1bdb2d8ec9e68ebf02eaec864c3d95
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-25 18:17:31 -0300

    button clear added

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let colorBase3 = document.querySelectorAll(".color")[2];
 let colorBase4 = document.querySelectorAll(".color")[3];
 let listaDePixels = document.querySelectorAll(".pixel");
 let clearButton = document.querySelector("button");
+let palette = document.querySelector("#color-palette");
+let pixelBoard = document.querySelector('#pixel-board');
+let corSelecionada = document.querySelector('#selected');
 
 //definições de cor prederminadas
 colorBase1.style.background = "black";
@@ -15,12 +18,21 @@ colorBase4.style.background = "pink";
 //funções
 function clear() {
 for (let i = 0; i < listaDePixels.length; i += 1){
-    listaDePixels[i].style.background = "rgb(255, 255, 255)"
+    listaDePixels[i].style.backgroundColor = "white";
     }
+}
+function colorSelect() {
+    palette.className = "selected";
+}
+function colorir() {
+    corSelecionada.style.backgroundColor = pixelBoard.target.style.backgroundColor;
 }
 
 
 //eventos
 clearButton.addEventListener('click', clear);
+palette.addEventListener('click', colorSelect);
+pixelBoard.addEventListener('click', colorir);
 
 //chamadas de funções
+clear()
