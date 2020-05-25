2f7c1ceb98996a182d8aa21ce3c4dcc29bc6a4ee
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-25 12:28:01 -0300

    criando funcao colorir

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ const pixel = document.querySelectorAll('.pixel');
 
 btn.addEventListener('click', function () {
   for (let i = 0; i < pixel.length; i+=1) {
-    pixel[i].className = 'pixel';
     pixel[i].style.backgroundColor = 'white';
   }
 })
 
-let cores = document.querySelectorAll('.color');
+const cores = document.querySelectorAll('.color');
+
 let colorPalette = document.querySelector('#color-palette');
 colorPalette.addEventListener('click', function (event) {
-  corEscolhida = event.target;
+  let corEscolhida = event.target;
   for (let i = 0; i < cores.length; i+=1) {
     cores[i].classList.remove('selected');
   }
   corEscolhida.classList.add('selected');
 })
 
-let trocaCor = document.querySelector('tbody');
-trocaCor.addEventListener('click', function (event) {
-  let trocaPixel = event.target;
+let colorir = document.querySelector('tbody');
+colorir.addEventListener('click', function (event) {
+  let pixelEscolhido = event.target;
+  for (let i = 0; i < cores.length; i+=1) {
+    if (cores[i].classList.className === 'selected') {
+      pixelEscolhido.style.backgroundColor = cores[i].style.backgroundColor;
+    }
+  }
 })
