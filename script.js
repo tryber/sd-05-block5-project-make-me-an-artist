24c9619e7932742f2009759a01d0b80791095371
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-25 00:18:51 -0300

    add elementos pixel como tabela

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-const lineOfPixels = document.createElement('section');
-lineOfPixels.className = 'linha';
-
 const emptyPixel = document.createElement('div');
 emptyPixel.className = 'pixel';
 
-
-function createLineOfPixel() {
-  document.querySelector('#pixel-board').appendChild;
-}
-
-
-createLineOfPixel();
-createLineOfPixel();
-createLineOfPixel();
-createLineOfPixel();
+const lineOfPixels = document.createElement('section');
+lineOfPixels.className = 'line';
