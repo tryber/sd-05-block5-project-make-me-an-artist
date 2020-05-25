d65bd2735ac7532e552a4e8e39f5bf940b454326
Mau <mauricio.sdjr@gmail.com>
2020-05-25 20:30:18 -0300

    pixel board refactored (dinamic now)

--- script.js
+++ script.js
@@ -1,29 +1,62 @@
+//Create pixel board
+let pixelBoard = document.getElementById("pixel-board");
+
+function createPixelsLine(numberOfPixels) {
+  for(let i = 0; i < numberOfPixels; i += 1) {
+  let pixelLine = document.createElement("tr");
+  pixelBoard.appendChild(pixelLine);
+  }
+}
+createPixelsLine(5);
+
+let tagTR = document.getElementsByTagName("tr");
+let numberTagTR;
+
+function createPixelsColumn(numberOfPixels) {
+  for(let i = 0; i < numberOfPixels; i += 1) {
+    for(let j = 0; j < tagTR.length; j += 1) {
+      let pixelColumn = document.createElement("td");
+      numberTagTR = tagTR[j];
+      numberTagTR.appendChild(pixelColumn);
+      pixelColumn.className = "pixel";
+      pixelColumn.addEventListener("click", paintPixel)
+    }
+  }
+}
+createPixelsColumn(5);
+
 //Change class "selected"
 let colorBlack = document.getElementById("cor1");
 let colorGreen = document.getElementById("cor2");
 let colorYellow = document.getElementById("cor3");
 let colorRed = document.getElementById("cor4");
-let colorSelected = colorBlack;
+let colorSelected = document.querySelector(".selected");
 let colorPalette = document.getElementById("color-palette");
 
 function changeSelected() {
-    if(event.target === colorBlack) {
-        colorSelected.classList.remove("selected");
-        colorBlack.classList.add("selected");
-        colorSelected = event.target;
-    } else if(event.target === colorGreen) {
-        colorSelected.classList.remove("selected");
-        colorGreen.classList.add("selected");
-        colorSelected = event.target;
-    } else if(event.target === colorYellow) {
-        colorSelected.classList.remove("selected");
-        colorYellow.classList.add("selected");
-        colorSelected = event.target;
-    } else if(event.target === colorRed) {
-        colorSelected.classList.remove("selected");
-        colorRed.classList.add("selected");
-        colorSelected = event.target;
-    }
+  if(event.target === colorBlack) {
+    colorSelected.classList.remove("selected");
+    colorBlack.classList.add("selected");
+    colorSelected = event.target;
+  } else if(event.target === colorGreen) {
+    colorSelected.classList.remove("selected");
+    colorGreen.classList.add("selected");
+    colorSelected = event.target;
+  } else if(event.target === colorYellow) {
+    colorSelected.classList.remove("selected");
+    colorYellow.classList.add("selected");
+    colorSelected = event.target;
+  } else if(event.target === colorRed) {
+    colorSelected.classList.remove("selected");
+    colorRed.classList.add("selected");
+    colorSelected = event.target;
+  }
 }
 
 colorPalette.addEventListener("click", changeSelected);
+
+//Paint pixels
+function paintPixel() {
+  
+}
+
