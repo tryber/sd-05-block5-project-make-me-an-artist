2db79498d344d8387b55c90e178e387b757975b6
Marylange <marylangess@gmail.com>
2020-05-25 14:49:03 -0300

    codeClimate

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// cria dinamicamente os pixels
 function criarQuadros() {
   const div1 = document.querySelector('.pixel-board');
 
-  for(let i = 0; i < 25; i += 1) {
+  for ( let i = 0; i < 25; i += 1 ) {
     const newDiv = document.createElement('div');
     newDiv.className = 'pixel';
     div1.appendChild(newDiv);
@@ -9,53 +10,55 @@ function criarQuadros() {
 }
 criarQuadros();
 
-//evento de click nas cores
-const coresBlack = document.querySelector('.selected');
-coresBlack.addEventListener('click', aplicarCorBlack);
-
+// evento de click nas cores
 let corAtual = 'black';
 
 function aplicarCorBlack() {
   corAtual = 'black';
 }
 
-const coresYellow = document.querySelector('.color-yellow');
-coresYellow.addEventListener('click', aplicarCorYellow);
+const coresBlack = document.querySelector('.selected');
+coresBlack.addEventListener('click', aplicarCorBlack);
+
 
 function aplicarCorYellow() {
   corAtual = 'yellow';
 }
 
-const coresBlue = document.querySelector('.color-blue');
-coresBlue.addEventListener('click', aplicarCorBlue);
+const coresYellow = document.querySelector('.color-yellow');
+coresYellow.addEventListener('click', aplicarCorYellow);
+
 
 function aplicarCorBlue() {
   corAtual = 'blue';
 }
 
-const coresGreen = document.querySelector('.color-green');
-coresGreen.addEventListener('click', aplicarCorGreen);
+const coresBlue = document.querySelector('.color-blue');
+coresBlue.addEventListener('click', aplicarCorBlue);
 
 function aplicarCorGreen() {
   corAtual = 'green';
 }
 
-//caixas de pixel em branco
-const pixels = document.querySelectorAll('.pixel');
-for(let i = 0; i < pixels.length; i += 1) {
-  pixels[i].addEventListener('click', identicarPixel);
-}
+const coresGreen = document.querySelector('.color-green');
+coresGreen.addEventListener('click', aplicarCorGreen);
 
+// caixas de pixel em branco
+const pixels = document.querySelectorAll('.pixel');
 function identicarPixel(evento) {
   evento.target.style.backgroundColor = corAtual;
 }
 
-//limpar cores
-let botaoApagarCores = document.querySelector('#clear-board');
+for ( let i = 0; i < pixels.length; i += 1 ) {
+  pixels[i].addEventListener('click', identicarPixel);
+}
+
+// limpar cores
+const botaoApagarCores = document.querySelector('#clear-board');
 botaoApagarCores.addEventListener('click', apagarEstilos);
 
 function apagarEstilos() {
-  for(let i = 0; i < pixels.length; i += 1) {
+  for ( let i = 0; i < pixels.length; i += 1 ) {
     document.querySelectorAll('.pixel')[i].style.backgroundColor = null;
   }
 }
