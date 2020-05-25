628442052e397a38ffbbbba61718c383c84149ea
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-05-25 16:32:22 -0300

    Resolvendo o item 7 do projeto

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 window.onload = function() {
-
-  criaBoard(5);
-
   function criaBoard(n) {
-    
-
     for (let i=1; i<=n; i+=1) {
-      let ultimaPosDentroBoard = document.getElementById("pixel-board");
-      let criaDivLinha = document.createElement("div");
-      criaDivLinha.className = "Linha" + i;
+      const ultimaPosDentroBoard = document.getElementById('pixel-board');
+      let criaDivLinha = document.createElement('div');
+      criaDivLinha.className = 'Linha' + i;
       ultimaPosDentroBoard.appendChild(criaDivLinha);
 
       for (let j = 1; j<=n; j+=1){
-        let criaColuna = document.createElement("div");
-        criaColuna.className = "pixel " + "Coluna"+j;
+        let criaColuna = document.createElement('div');
+        criaColuna.className = 'pixel ' + 'Coluna'+j;
         let ultimaPosDentroLinha = ultimaPosDentroBoard.lastElementChild;
         ultimaPosDentroLinha.appendChild(criaColuna); 
       }
-      
     }
   }
-}
+  function selecionaCorPaleta (event) {
+    let pegaPosSelecionada = event.target;
+    let pegaCorSelecionada = getComputedStyle(event.target, null).getPropertyValue('background-color');
+    let pegaClasseSelecionada = event.target.className;
+    console.log(pegaPosSelecionada);
+    console.log(pegaCorSelecionada);
+    console.log(pegaClasseSelecionada);
+    if (pegaClasseSelecionada !== "color selected") {
+      let elementosPaleta = document.getElementById('color-palette').children;
+      for (let i=0; i<elementosPaleta.length; i+=1){
+        if(elementosPaleta[i].className === 'color selected') {
+          elementosPaleta[i].className = 'color';
+        }
+      }
+      event.target.className = 'color selected';
+    }
+  }
+  let monitoraColorPalette = document.getElementById("color-palette");
+  monitoraColorPalette.addEventListener("click", selecionaCorPaleta);
+
+  criaBoard(5);
+  }
