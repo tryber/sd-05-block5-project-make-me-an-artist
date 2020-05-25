f0b6c453fed0f570274d255a3638380158a4ba86
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-25 01:51:46 -0300

    testando botao limpar

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const pixel = document.querySelectorAll('.pixel');
 
 btn.addEventListener('click', function () {
   for (let i = 0; i < pixel.length; i+=1) {
+    pixel[i].className = 'pixel';
     pixel[i].style.backgroundColor = 'white';
   }
 })
