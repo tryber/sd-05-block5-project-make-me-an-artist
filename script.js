88d3f1f6c4b8c6e0123d74d56dd36c2f1779f9df
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-25 02:23:13 -0300

    criando funcao que coloca a classe selected na cor selecionada e tira da cor anterior

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ btn.addEventListener('click', function () {
     pixel[i].style.backgroundColor = 'white';
   }
 })
+
+let cores = document.querySelectorAll('.color');
+let colorPalette = document.querySelector('#color-palette');
+colorPalette.addEventListener('click', function (event) {
+  corEscolhida = event.target;
+  for (let i = 0; i < cores.length; i+=1) {
+    cores[i].classList.remove('selected');
+  }
+  corEscolhida.classList.add('selected');
+})
+
+let trocaCor = document.querySelector('tbody');
+trocaCor.addEventListener('click', function (event) {
+  let trocaPixel = event.target;
+})
