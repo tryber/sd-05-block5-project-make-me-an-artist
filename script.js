3b16ca772be935807da3621009a35b7684344b99
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-25 15:22:29 -0300

    corrigindo erros do code climate

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.querySelector('#clear-board');
 const pixel = document.querySelectorAll('.pixel');
 
 btn.addEventListener('click', function () {
-  for (let i = 0; i < pixel.length; i+=1) {
+  for (let i = 0; i < pixel.length; i += 1) {
     pixel[i].className = 'pixel';
   }
 })
@@ -12,7 +12,7 @@ const cores = document.querySelectorAll('.color');
 const colorPalette = document.querySelector('#color-palette');
 colorPalette.addEventListener('click', function (event) {
   let corEscolhida = event.target;
-  for (let i = 0; i < cores.length; i+=1) {
+  for (let i = 0; i < cores.length; i += 1) {
     cores[i].classList.remove('selected');
   }
   corEscolhida.classList.add('selected');
@@ -23,7 +23,7 @@ colorir.addEventListener('click', function (event) {
   let pixelEscolhido = event.target;
   pixelEscolhido.className = ' ';
   let lista = document.querySelector('.selected').classList;
-  for (let i = 0; i < lista.length; i+=1) {
+  for (let i = 0; i < lista.length; i += 1) {
     if (lista[i] !== 'color' && lista[i] !== 'selected') {
       pixelEscolhido.classList.add(lista[i]);
     }
