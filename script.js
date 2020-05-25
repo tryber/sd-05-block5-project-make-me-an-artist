c4a95e1bfb2acbeea9498c5c7e91f7fae60f4e1a
matambrosio <matmagamb1@gmail.com>
2020-05-25 19:07:13 -0300

    req 4 tentativa 3

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-let pallete = document.querySelector("#color-palette");
-pallete.addEventListener("mouseover", palleteColors)
+let palette = document.querySelector("#color-palette");
+palette.addEventListener("click", myClickpalette);
 
-function palleteColors(eventoPallete){
-  console.log(eventoPallete.target);
+function myClickpalette(paletteEvent){
+ classCopy = paletteEvent.target.className.split(" ")
+ console.log(classCopy);
+ return(classCopy)
+}
+//------------------------------------------------------------
+let board = document.querySelector("#pixel-board")
+board. addEventListener("click", myPasteColor);
+function myPasteColor(selectedPaste){
+  console.log(selectedPaste.target)
+  let a =selectedPaste.target.className.split(" ");
+
+  if(selectedPaste.target.className !== "linha"){
+    a[1] = classCopy[1]
+    a = a[0] + " " + a[1];
+    selectedPaste.target.className = a;
+    console.log(a)
+   }
+  
+   return(a);
+}
+
+//------------------------------------------------------------
+let clearBt = document.querySelector("#clear-board");
+clearBt.addEventListener("click", clearAllPixels);
+function clearAllPixels(){
+  console.log(a)
+
 }
