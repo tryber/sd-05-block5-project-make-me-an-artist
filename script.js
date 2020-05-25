5dfaaa6a3416929834d0e189ac415b619d0a8f04
Christian Duessel <duessel@gmail.com>
2020-05-25 15:59:44 -0300

    pincel cor selecionada

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const btn = document.getElementById("clear-board");
 
 function limpa (){
-  document.getElementById("pi1").style.backgroundColor = "white";
+  document.getElementById("pi1").style.backgroundColor = 'rgb(0, 0, 255';
   document.getElementById("pi2").style.backgroundColor = "white";
   document.getElementById("pi3").style.backgroundColor = "white";
   document.getElementById("pi4").style.backgroundColor = "white";
@@ -33,9 +33,29 @@ function limpa (){
 btn.addEventListener("click", limpa);
 
 // clicar na cor e pintar o pixel
-//
+
+document.getElementById("cor1").addEventListener("click", function(){
+    document.getElementById("corselecionada").style.backgroundColor= "black";
+    alert("cor selecionada preto");
+    });
+
+document.getElementById("cor2").addEventListener("click", function(){
+    document.getElementById("corselecionada").style.backgroundColor="blue";
+    alert("cor selecionada azul");
+});
+
+document.getElementById("cor3").addEventListener("click", function(){
+    document.getElementById("corselecionada").style.backgroundColor="red";
+    alert("cor selecionada vermelho");
+});
+
+document.getElementById("cor4").addEventListener("click", function(){
+    document.getElementById("corselecionada").style.backgroundColor="green";
+    alert("cor selecionada verde");
+});
+                    
 // document.addEventListener('click', function (event) {
-//     if ( event.target.classList.contains( 'accordion-link' ) ) {
-//         // Do something...
+//     if ( event.target.classList.contains( 'color' ) ) {
+//     document.getElementById("corselecionada").style.backgroundColor
 //     }
 // }, false);
