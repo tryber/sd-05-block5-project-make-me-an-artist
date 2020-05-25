3fbc82916580584dd76e156559ca917e93a17719
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-25 17:55:47 -0300

    modificando functions com melhor sintax para event.target

--- script.js
+++ script.js
@@ -2,19 +2,16 @@
 //Ao clicar em uma das cores da paleta, a cor selecionada é que vai ser usada para preencher os pixels no quadro.
 // Somente uma cor da paleta de cores tem a classe `selected` de cada vez
 // Os pixels dentro do quadro não podem ter a classe `selected` quando são clicados
+let currentSelected = document.getElementsByClassName("selected")[0];
 
-function changeSelected(ecolor) { //function deve transferir a class selected
-  let colorSelected = ecolor.target;
-  if (ecolor.target != document.getElementsByClassName("selected")[0]) {
-    document.getElementsByClassName("selected")[0].className -= "selected";
-  }
-  else {
+function changeSelected(event) { //function deve transferir a class selected
+  let colorSelected = event.target;
+  if (colorSelected != currentSelected) {
+    currentSelected.className -= "selected";
     colorSelected.className += " selected";
+    currentSelected = document.getElementsByClassName("selected")[0];
   }
-
 }
-
-colorSelected.addEventListener("click", selectColor);
 
 // Ao clicar em um pixel dentro do quadro após selecionar uma cor na paleta, o pixel deve ser preenchido com esta cor.
 // - Ao carregar a página deve ser possível pintar os pixels de preto
@@ -23,11 +20,9 @@ colorSelected.addEventListener("click", selectColor);
 // - Somente o pixel que foi clicado deverá ser preenchido com a cor selecionada, sem influenciar na cor dos demais pixels.
 // (ou seja nao usar a class que é igual para todas, e sim um event.target).
 
-let currentSelected = document.getElementsByClassName("selected")[0];
-
-function giveColor(epixel) {
-  let selectedPixel = epixel.target;
-  selectedPixel.style.backgroundColor = currentSelectedstyle.backgroundColor;
+function giveColor(eventpixel) {
+  let selectedPixel = eventpixel.target;
+  selectedPixel.style.backgroundColor = currentSelected.value;
 }
 selectedPixel.addEventListener("click", giveColor);
 
