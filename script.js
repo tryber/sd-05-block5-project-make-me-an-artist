f0b27a9cc28864a9d54592354a6b291173d260a7
Gui-Alucard <guilhermeanv7822@gmail.com>
2020-05-25 17:30:40 -0300

    Added the title and 4 colors within the palett boxes.

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('blackColor').style.backgroundColor = 'black';
+document.getElementById('redColor').style.backgroundColor = 'red';
+document.getElementById('blueColor').style.backgroundColor = 'blue';
+document.getElementById('greenColor').style.backgroundColor = 'green';
+
+
