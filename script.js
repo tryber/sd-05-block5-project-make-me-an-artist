a85f3ed5efd05b91907f94e9a4461fdd2d3b2678
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-05-25 18:38:16 -0300

    arrumei a function changeSelected

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ let colorSelected = "";
 function changeSelected(event) { //function deve transferir a class selected
   let colorSelected = event.target;
   if (colorSelected != currentSelected) {
-    currentSelected.className -= "selected";
-    colorSelected.className += " selected";
+    currentSelected.classList.remove('selected');
+    colorSelected.classList.add('selected');
     currentSelected = document.getElementsByClassName("selected")[0];
   }
 }
@@ -22,14 +22,13 @@ orange.addEventListener("click", changeSelected);
 green.addEventListener("click", changeSelected);
 blue.addEventListener("click", changeSelected);
 
-
-let board = document.querySelector(".board");
 // Ao clicar em um pixel dentro do quadro após selecionar uma cor na paleta, o pixel deve ser preenchido com esta cor.
 // - Ao carregar a página deve ser possível pintar os pixels de preto
 // (ou seja, interpretando: é o selected que conta para isso)
 // - Após selecionar uma outra cor na paleta, deve ser possível pintar os pixels com essa cor
 // - Somente o pixel que foi clicado deverá ser preenchido com a cor selecionada, sem influenciar na cor dos demais pixels.
 // (ou seja nao usar a class que é igual para todas, e sim um event.target).
+let board = document.querySelector(".board");
 let selectedPixel = "";
 function giveColor(eventpixel) {
   let selectedPixel = eventpixel.target;
