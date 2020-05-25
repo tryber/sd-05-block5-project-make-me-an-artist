a98fac505a31902cf8d61ad059ef9dc5d692da1c
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-05-25 18:52:03 -0300

    Resolvendo o item 8 e 9 do projeto

--- script.js
+++ script.js
@@ -1,38 +1,65 @@
-window.onload = function() {
+window.onload = function () {
+
+  let selecionaPreto = document.getElementById("preto");
+  let pegaCorSelecionada = getComputedStyle(selecionaPreto, null).getPropertyValue('background-color');
+  let monitoraColorPalette = document.getElementById("color-palette");
+  monitoraColorPalette.addEventListener("click", selecionaCorPaleta);
+  //monitorar "elements" nao deu certo - descobrir pq.
+  //let monitoraPixelBoard = document.getElementsByClassName('pixel');
+  //for (let i=0; i<monitoraPixelBoard.length; i+=1) {
+  //  monitoraPixelBoard[i].addEventListener("click", encheCor(event));
+  //  console.log(monitoraPixelBoard[i]);
+  //}
+  let monitoriaLimpar = document.getElementById('clear-board');
+  monitoriaLimpar.addEventListener('click', limpaTudo);
+
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('pixel')) {
+      event.target.style.backgroundColor = pegaCorSelecionada;
+    }
+  }, false);
+
+  criaBoard(5);
+
   function criaBoard(n) {
-    for (let i=1; i<=n; i+=1) {
+    for (let i = 1; i <= n; i += 1) {
       const ultimaPosDentroBoard = document.getElementById('pixel-board');
       let criaDivLinha = document.createElement('div');
       criaDivLinha.className = 'Linha' + i;
       ultimaPosDentroBoard.appendChild(criaDivLinha);
 
-      for (let j = 1; j<=n; j+=1){
+      for (let j = 1; j <= n; j += 1) {
         let criaColuna = document.createElement('div');
-        criaColuna.className = 'pixel ' + 'Coluna'+j;
+        criaColuna.className = 'pixel';
+        criaColuna.className = 'pixel ' + 'Coluna' + j;
         let ultimaPosDentroLinha = ultimaPosDentroBoard.lastElementChild;
-        ultimaPosDentroLinha.appendChild(criaColuna); 
+        ultimaPosDentroLinha.appendChild(criaColuna);
       }
     }
   }
-  function selecionaCorPaleta (event) {
+  function selecionaCorPaleta(event) {
     let pegaPosSelecionada = event.target;
-    let pegaCorSelecionada = getComputedStyle(event.target, null).getPropertyValue('background-color');
+    pegaCorSelecionada = getComputedStyle(event.target, null).getPropertyValue('background-color');
     let pegaClasseSelecionada = event.target.className;
-    console.log(pegaPosSelecionada);
-    console.log(pegaCorSelecionada);
-    console.log(pegaClasseSelecionada);
     if (pegaClasseSelecionada !== "color selected") {
       let elementosPaleta = document.getElementById('color-palette').children;
-      for (let i=0; i<elementosPaleta.length; i+=1){
-        if(elementosPaleta[i].className === 'color selected') {
+      for (let i = 0; i < elementosPaleta.length; i += 1) {
+        if (elementosPaleta[i].className === 'color selected') {
           elementosPaleta[i].className = 'color';
         }
       }
       event.target.className = 'color selected';
     }
   }
-  let monitoraColorPalette = document.getElementById("color-palette");
-  monitoraColorPalette.addEventListener("click", selecionaCorPaleta);
 
-  criaBoard(5);
+  function encheCor(event) {
+    event.target.style.backgroundColor = pegaCorSelecionada;
   }
+
+  function limpaTudo() {
+    let pegaPosicaoPixels = document.getElementsByClassName("pixel");
+    for (let i=0; i<pegaPosicaoPixels.length; i+=1){
+      pegaPosicaoPixels[i].style.backgroundColor = "white";
+    }
+  }
+}
