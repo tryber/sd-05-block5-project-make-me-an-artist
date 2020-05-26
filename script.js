462bcc84234df6af06957bb274f03617db4d954a
marinabrcls <barcelos.marina@gmail.com>
2020-05-26 02:26:20 -0300

    Agora funciona a função de selecionar cor e o botão de apagar

--- script.js
+++ script.js
@@ -6,30 +6,48 @@ let color3 = document.querySelector(".color-3");
 let colorPalette = document.getElementById("color-palette")
 let pixelBoard = document.getElementById("pixel-board");
 let botaoLimpar = document.getElementById("clear-board");
-let selected = black;
+let corSelecionada = black;
 
 //Funções
 
 //Essa é pra selecionar uma cor da paleta de cores
 function changeSelected() {
   if (event.target === black) {
-    selected.classList.remove("selected");
+    corSelecionada.classList.remove("selected");
     black.classList.add("selected");
-    selected = event.target;
+    corSelecionada = event.target;
   } else if (event.target === color1) {
-    selected.classList.remove("selected");
+    corSelecionada.classList.remove("selected");
     color1.classList.add("selected");
-    selected = event.target;
+    corSelecionada = event.target;
   } else if (event.target === color2) {
-    selected.classList.remove("selected");
+    corSelecionada.classList.remove("selected");
     color2.classList.add("selected");
-    selected = event.target;
+    corSelecionada = event.target;
   } else if (event.target === color3) {
-    selected.classList.remove("selected");
+    corSelecionada.classList.remove("selected");
     color3.classList.add("selected");
-    selected = event.target;
+    corSelecionada = event.target;
+  }
+}
+
+//Essa é pra colorir um pixal quando clicar
+function colorPixel() {
+  let currentColor = corSelecionada.classList[1];
+  event.target.classList.remove(event.target.classList[1]);
+  event.target.classList.add(currentColor);
+}
+
+//Essa é pra fazer funcionar o botão limpar
+function limparTudo() {
+  for (i = 0; i < pixelBoard.children.length; i++) {
+    let colorClass = pixelBoard.children[i].classList[1];
+    pixelBoard.children[i].classList.remove(colorClass);
+    pixelBoard.children[i].classList.add("blank");
   }
 }
 
 //Event Listeners
 colorPalette.addEventListener("click", changeSelected);
+pixelBoard.addEventListener("click", colorPixel);
+botaoLimpar.addEventListener("click", limparTudo);
