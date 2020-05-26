56d11a12358446e20603222fb06c32c3307ee63c
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-26 00:11:58 -0300

    corrigindo erros do code climate

--- script.js
+++ script.js
@@ -5,28 +5,28 @@ btn.addEventListener('click', function () {
   for (let i = 0; i < pixel.length; i += 1) {
     pixel[i].className = 'pixel';
   }
-})
+});
 
 const cores = document.querySelectorAll('.color');
 
 const colorPalette = document.querySelector('#color-palette');
 colorPalette.addEventListener('click', function (event) {
-  let corEscolhida = event.target;
+  const corEscolhida = event.target;
   for (let i = 0; i < cores.length; i += 1) {
     cores[i].classList.remove('selected');
   }
   corEscolhida.classList.add('selected');
-})
+});
 
 const colorir = document.querySelector('tbody');
 colorir.addEventListener('click', function (event) {
-  let pixelEscolhido = event.target;
+  const pixelEscolhido = event.target;
   pixelEscolhido.className = ' ';
-  let lista = document.querySelector('.selected').classList;
+  const lista = document.querySelector('.selected').classList;
   for (let i = 0; i < lista.length; i += 1) {
     if (lista[i] !== 'color' && lista[i] !== 'selected') {
       pixelEscolhido.classList.add(lista[i]);
     }
   }
   pixelEscolhido.classList.add('pixel');
-})
+});
