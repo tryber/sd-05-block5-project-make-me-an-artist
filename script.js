66de985098bd16e884e62ac9dabf126688fd3b7e
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-26 19:30:48 -0300

    Funcao para selecionar a cor da paleta

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+// Definição das variáveis
 const black = document.getElementById('black');
 const first = document.getElementById('first');
 const second = document.getElementById('second');
 const third = document.getElementById('third');
+const colorPalette = document.getElementById('color-palette');
+let corAtual = black;
+
+// Funções
+function selectColor() {
+  corAtual.classList.remove('selected');
+  event.target.classList.add('selected');
+  corAtual = event.target;
+}
+
+// Event Listeners
+colorPalette.addEventListener('click' , selectColor);
