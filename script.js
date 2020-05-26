9af2ecd69a73e18e734e1904ffead1989ccf83ef
pauloricardoz <trybe.przt@gmail.com>
2020-05-26 20:56:22 -0300

    paint working

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ sessionStorage.setItem("selected", "color1")
 
 let colorSelected = sessionStorage.getItem("selected")
 let paleta = document.getElementById("color-palette");
-let pixelBoard =document.getElementById("pixel-board")
+let pixelBoard = document.getElementById("pixel-board")
+let colorArray = {
+  'color1': 'black',
+  'color2': 'red',
+  'color3': 'blue',
+  'color4': 'green'
+}
 
 paleta.addEventListener("click", function (evento) {
   if (evento.target.className.toString().indexOf("color") >= 0) {
@@ -19,10 +25,10 @@ function changeColorSelected(oldColor, newColor) {
   elementNew.className += "selected"
   sessionStorage.setItem("selected", elementNew.className.toString().substr(elementNew.className.toString().lastIndexOf("color"), 6))
 }
-/*
-pixelBoard.addEventListener("click",function(evento){
-    if(evento.target.className.toString().indexOf("pixel")>=0)
-      console.log(evento.target+=" color1")
+
+pixelBoard.addEventListener("click", function (evento) {
+  if (evento.target.className.toString().indexOf("pixel") >= 0)
+    evento.target.style.backgroundColor = colorArray[sessionStorage.getItem("selected")]    
 });
-*/
+
 
