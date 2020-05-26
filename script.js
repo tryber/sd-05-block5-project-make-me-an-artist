ef5b55e7461e06d3f821b462dd6bfde558665170
Larissa Palombo <larissampalombo@gmail.com>
2020-05-26 16:45:05 -0300

    comecando js

---
+++ script.js
@@ -0,0 +1,19 @@
+const cores = document.getElementsByClassName[];
+const botao = document.getElementById('clear-board');
+let sqr = "";
+const quadrado = document.getElementById ('pixel-board')
+
+cores.coresaddEventListener("click", selecionaCor);
+botao.addEventListener ("click", limpar);
+
+function selecionarCor () {
+    if 
+}
+
+function limpar (){
+    sqr = quadrado.firstChild;
+    for (let a = 1; a <= quadrado.children.length; a += 1){
+        sqr.style = 'backgroung-color:  white';
+        sqr[] += 1;
+    }
+}
