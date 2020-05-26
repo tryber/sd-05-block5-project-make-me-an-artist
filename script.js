b6863a55a5d1fcf9bf39ace057fc0e1414465109
Erlingsjunior <erlingsjunior@gmail.com>
2020-05-26 19:23:36 -0300

    alteração de Js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+//variaveis constantes
 const colorBlack = document.querySelector('.black');
 const colorRed = document.querySelector('.red');
 const colorGreen = document.querySelector('.green');
 const colorBlue = document.querySelector('.blue');
 const pixelBoard = document.getElementById('pixel-board');
-const clearBoard = document.getElementById('clear-board');
-
-
+//variavel que muda
 let corSelecionada = document.querySelector('.selected');
 let corEscolhida = corSelecionada.classList[1];
-
+//função de selecionar a cor
 function selecionaCor(){
     let seleciona = event.target
     seleciona.classList.add('selected')
@@ -16,13 +15,21 @@ function selecionaCor(){
     corEscolhida = seleciona.classList[1]
     corSelecionada = seleciona;
 }
-
+//função para pintar com a cor selecionada
 function pintaCor(){
     let seleciona = event.target
     seleciona.classList.remove(seleciona.classList[1])
     seleciona.classList.add(corEscolhida);
 }
-
+//botão de limpar o quadro
+const clearButton = document.getElementById('clear-board');
+clearButton.addEventListener('click', function () {
+  const pixelDivs = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixelDivs.length; i += 1) {
+    pixelDivs[i].style.backgroundColor = 'white';
+  }
+});
+//eventos
 colorBlack.addEventListener('click', selecionaCor);
 colorRed.addEventListener('click', selecionaCor);
 colorGreen.addEventListener('click', selecionaCor);
