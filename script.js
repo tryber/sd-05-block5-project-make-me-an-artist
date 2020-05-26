03dde1cb685a5bd5e67daadef030cb35da061960
pauloricardoz <trybe.przt@gmail.com>
2020-05-26 18:01:39 -0300

    testing

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+
 if (sessionStorage.key("selected").toString().indexOf("color") < 0) {
   sessionStorage.setItem("selected", "color1")
 }
 
 let colorSelected = sessionStorage.getItem("selected")
 let paleta = document.getElementById("color-palette");
-
+let pixelBoard=document.getElementById("pixel-board");
 paleta.addEventListener("click", function (evento) {
   if (evento.target.className.toString().indexOf("color") >= 0) {
     changeColorSelected(colorSelected, evento.target.className);
     //console.log(document.getElementsByClassName("color"))
   }
 });
+
 function changeColorSelected(oldColor, newColor) {
   let elementOld = document.getElementsByClassName("selected")[0];
   let elementNew = document.getElementsByClassName(newColor)[0];
