71df998444caf59f46089457801b1928e931e638
pauloricardoz <trybe.przt@gmail.com>
2020-05-26 16:34:00 -0300

    7- Pushing class selected to a new element um the color selection, selected color saved in the sessionStorage, function to change color selected implemented in changeColor, condicion to choose only divs with color class inside.

---
+++ script.js
@@ -0,0 +1,21 @@
+if (sessionStorage.key("selected").toString().indexOf("color") < 0) {
+  sessionStorage.setItem("selected", "color1")
+}
+
+let colorSelected = sessionStorage.getItem("selected")
+let paleta = document.getElementById("color-palette");
+
+paleta.addEventListener("click", function (evento) {
+  if (evento.target.className.toString().indexOf("color") >= 0) {
+    changeColorSelected(colorSelected, evento.target.className);
+    //console.log(document.getElementsByClassName("color"))
+  }
+});
+
+function changeColorSelected(oldColor, newColor) {
+  let elementOld = document.getElementsByClassName("selected")[0];
+  let elementNew = document.getElementsByClassName(newColor)[0];
+  elementOld.className = elementOld.className.replace("selected", "")
+  elementNew.className += "selected"
+  sessionStorage.setItem("selected", elementNew.className.toString().substr(elementNew.className.toString().lastIndexOf("color"), 6))
+}
