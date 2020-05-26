470a3cf3da95ecba9c5d2d77642e06262473830b
RenataNovais <rnovaiscs@gmail.com>
2020-05-26 00:09:16 -0300

    Code that changes the selected class when users clicks on color-palette option. Code that changes the color of each pixel as users clicks on it, with the color choosen on color-palette

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-let colorSelected = 'black';
+let colorBlack = document.getElementById('colorOne');
+let colorRed = document.getElementById('colorTwo');
+let colorBlue = document.getElementById('colorThree');
+let colorGreen = document.getElementById('colorFour');
+let colorSelected = document.querySelector('.selected');
+let pixelUnit = document.querySelectorAll('.pixel');
+let color = "black";
 
-function selectColor (colorClicked) {
-    colorSelected = colorClicked;
-    console.log(colorSelected);
+colorBlack.addEventListener('click', selectedColor);
+colorRed.addEventListener('click', selectedColor);
+colorBlue.addEventListener('click', selectedColor);
+colorGreen.addEventListener('click', selectedColor);
+
+function selectedColor(event) {
+  if (event.target === colorBlack) {
+    colorSelected.classList.remove("selected");
+    colorBlack.classList.add("selected");
+    colorSelected = document.querySelector('.selected');
+    color = "black";
+  } else if (event.target === colorRed) {
+    colorSelected.classList.remove("selected");
+    colorRed.classList.add("selected");
+    colorSelected = document.querySelector('.selected');
+    color = "red";
+  } else if (event.target === colorBlue) {
+    colorSelected.classList.remove("selected");
+    colorBlue.classList.add("selected");
+    colorSelected = document.querySelector('.selected');
+    color = "blue";
+  } else if (event.target === colorGreen) {
+    colorSelected.classList.remove("selected");
+    colorGreen.classList.add("selected");
+    colorSelected = document.querySelector('.selected');
+    color = "green";
+  }
+  console.log(colorSelected);
+}
+
+
+for (let i = 0; i < pixelUnit.length; i += 1) {
+  pixelUnit[i].addEventListener('click', function() {
+    pixelUnit[i].style.backgroundColor = color;
+  })
 }
