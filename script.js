8d308f3e02f9fca2a80b414b11504a0009950043
Larissa Palombo <larissampalombo@gmail.com>
2020-05-26 18:32:34 -0300

    parte do selecionar e pintar quadrado

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-const cores = document.getElementsByClassName('color');
-const botao = document.getElementById('clear-board');
+const cores = document.getElementsByClassName ('color');
+const botao = document.getElementById ('clear-board');
+const cor1 = document.getElementById ('sqr1');
+const cor2 = document.getElementById ('sqr2');
+const cor3 = document.getElementById ('sqr3');
+const cor4 = document.getElementById ('sqr4');
 let sqr = "";
-const quadrado = document.getElementById ('pixel-board')
+const quadrado = document.getElementsByClassName ('pixel');
 
-cores.coresaddEventListener("click", selecionaCor);
+cor1.addEventListener("click", selecionaCor);
+cor2.addEventListener("click", selecionaCor);
+cor3.addEventListener("click", selecionaCor);
+cor4.addEventListener("click", selecionaCor);
 botao.addEventListener ("click", limpar);
 
-function selecionarCor () {
-    if 
+function selecionaCor (event) {
+    if (event.target === cor1) {
+        sqr = "black";
+    }
+    else if (event.target === cor2) {
+        sqr = "darkmagenta";
+    }
+    else if (event.target === cor3) {
+        sqr = "yellow";
+    }
+    else if (event.target === cor4){
+        sqr = "lightseagreen";
+    }
+}
+
+function mudaCor () {
+    quadrado.style.getElementsByClassName = "background-color: sqr";
 }
 
 function limpar (){
