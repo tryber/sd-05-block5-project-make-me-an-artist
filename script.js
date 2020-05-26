737b569d6ce18408dfb6b40321bd363e2b131acd
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-05-26 18:34:07 -0300

    criação funções js para seleção e mudança de cores

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
+let corPixel = '';
+function corSelecionada(e) {
+  let selecao = e.target;
+  let removerSelected = document.querySelectorAll('.color');
+  for(let i = 0; i < removerSelected.length; i+=1) {
+    removerSelected[i].classList.remove('selected');
+  }
+
+  selecao.classList.add('selected');
+  corPixel = getComputedStyle(selecao, null).getPropertyValue('background-color');
+  
+}
+
+const acessarPalette = document.querySelector('#color-palette');
+acessarPalette.addEventListener('click', corSelecionada);
+
+function mudaCor (e) {
+  const pixelQuadro = e.target;
+  pixelQuadro.style.backgroundColor = corPixel;
+}
+
+const pixelQuadro = document.querySelector('#pixel-board');
+pixelQuadro.addEventListener('click', mudaCor);
+
 
 
