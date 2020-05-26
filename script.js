e11f5b693ad168550e8fbda8e446a361d461de69
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-26 18:21:43 -0300

    Adiçao tabela e classes para paleta

---
+++ script.js
@@ -0,0 +1,4 @@
+const black = document.getElementById('black');
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+const third = document.getElementById('third');
