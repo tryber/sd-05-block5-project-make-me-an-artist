40edca8210de892126c5d1ca92a21244640cdee6
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-05-26 19:26:02 -0300

    correção cor inicial e code climate

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-let corPixel = '';
+let corPixel = 'black';
 function corSelecionada(e) {
-  let selecao = e.target;
-  let removerSelected = document.querySelectorAll('.color');
-  for(let i = 0; i < removerSelected.length; i+=1) {
+  const selecao = e.target;
+  const removerSelected = document.querySelectorAll('.color');
+  for (let i = 0; i < removerSelected.length; i += 1) {
     removerSelected[i].classList.remove('selected');
   }
 
   selecao.classList.add('selected');
-  corPixel = getComputedStyle(selecao, null).getPropertyValue('background-color');
-  
+  corPixel = getComputedStyle(selecao, null).getPropertyValue('background-color');  
 }
 
 const acessarPalette = document.querySelector('#color-palette');
 acessarPalette.addEventListener('click', corSelecionada);
 
-function mudaCor (e) {
+function mudaCor(e) {
   const pixelQuadro = e.target;
   pixelQuadro.style.backgroundColor = corPixel;
 }
 
 const pixelQuadro = document.querySelector('#pixel-board');
 pixelQuadro.addEventListener('click', mudaCor);
-
-
-
