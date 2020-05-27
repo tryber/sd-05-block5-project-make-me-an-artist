a5e3f9bf483eaf5e07b1a14f6f532e128640196d
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-27 17:52:50 -0300

    Set first selected color

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ let redColor = document.getElementsByClassName("red")[0];
 let blueColor = document.getElementsByClassName("blue")[0];
 let greenColor = document.getElementsByClassName("green")[0];
 
+blackColor.className = "selected";
 
+let selectedColor = document.getElementsByClassName("selected");
+ 
