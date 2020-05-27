cf4002671a404fee5213becb9cb2f283cb676388
hleoc <informig@gmail.com>
2020-05-27 17:25:42 -0300

    Requisito 4 teste 7

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+
+    //const caixaUm = document.querySelector('.color-black');
+    //const caixaDois = document.querySelector('color-red');
+    //const caixaTres = document.querySelector('color-blue');
+    //const caixaQuatro = document.querySelector('color-green');
+    const botaoMudaCor = document.querySelector('#clear-board');
+ 
+    function trocaCorBranca () {
+        let pixels = document.querySelectorAll('.pixel')
+        for (let c = 0; c < pixels.length; c+=1) {
+            pixels[c].style.backgroundColor = 'white'
+        }
+    }
+
+    botaoMudaCor.addEventListener('click', trocaCorBranca);
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
