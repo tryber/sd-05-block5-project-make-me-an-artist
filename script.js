c39f83949f8aa66a8046f7615eca197a5640cc98
Carla Nakajuni <carlanakajuni@hotmail.com>
2020-05-27 19:12:12 -0300

    Pintando os pixels

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Deixando a classe 'selected' em um elemento por vez.
 let coresPaletta = document.getElementById("color-palette");
-
+coresPaletta.addEventListener('click', trocandoClasseDasCores);
 function trocandoClasseDasCores(loucura) {
   if ((loucura.target.className).indexOf("color") >= 0) {
     for(let index = 0; index < 4; index+=1){
@@ -9,14 +9,36 @@ function trocandoClasseDasCores(loucura) {
     loucura.target.className += " selected";
   }
 }
-coresPaletta.addEventListener('click', trocandoClasseDasCores);
+
+// Pintando o board
+
+let board = document.getElementById("pixel-board");
+
+board.addEventListener('click', pintandoOPixel);
+function pintandoOPixel(evento) {
+  let corSelecionada = document.getElementsByClassName("selected")[0];
+  let cssProp = window.getComputedStyle(corSelecionada, null).getPropertyValue("background-color");
+  if(evento.target.className.indexOf("pixel") >= 0) {
+    evento.target.style.backgroundColor = cssProp;
+  }
+  console.log(cssProp);
+}
+
 
 // Botão para limpar o board
-let botao = document.getElementById("clear-board");
-let board = document.getElementsByClassName("pixel");
-let white = document.getElementById("white");
+// let botao = document.getElementById("clear-board");
+// let white = document.getElementById("white");
 
-function botaoParaLimparCores() {
-  board.innerHTML = botao.replaceWith.white
-}
-botao.addEventListener('click', botaoParaLimparCores);
+// function botaoParaLimparCores() {
+//   let pixels = document.getElementsByClassName("pixel");
+//   console.log(pixels[0])
+//   for(let i = 0; i < pixels.length; i++) {
+//     let temp = document.createElement("td")
+//     temp.className = "pixel"
+//     temp.style.backgroundColor = "white";
+//   console.log(document.querySelectorAll("td.pixel")[i] = temp);
+//   document.querySelectorAll("td.pixel")[i]
+//   }
+//   board.innerHTML = botao.replaceWith.white
+// }
+// botao.addEventListener('click', botaoParaLimparCores);
