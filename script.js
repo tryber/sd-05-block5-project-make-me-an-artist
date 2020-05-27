c15d2f17d63ddd3cc9db548d95b1e2707029f7e2
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-27 18:04:50 -0300

    code climate adjusted

--- script.js
+++ script.js
@@ -3,30 +3,33 @@
 let colorBase1 = document.querySelectorAll(".color")[0];
 let colorBase2 = document.querySelectorAll(".color")[1];
 let colorBase3 = document.querySelectorAll(".color")[2];
-let colorBase4 = document.querySelectorAll(".color")[3];
-let listaDePixels = document.querySelectorAll(".pixel");
-let clearButton = document.querySelector("button");
-let palette = document.querySelector("#color-palette");
+let colorBase4 = document.querySelectorAll('.color')[3];
+let listaDePixels = document.querySelectorAll('.pixel');
+let clearButton = document.querySelector('button');
+let palette = document.querySelector('#color-palette');
 let pixelBoard = document.querySelector('#pixel-board');
 let corSelecionada = document.querySelector('#selected');
-colorBase1.className = "color selected";
+colorBase1.className = 'color selected';
 
 // definições prederminadas
-colorBase1.style.background = "black";
-colorBase2.style.background = "green";
-colorBase3.style.background = "red";
-colorBase4.style.background = "pink";
+colorBase1.style.background = 'black';
+colorBase2.style.background = 'green';
+colorBase3.style.background = 'red';
+colorBase4.style.background = 'pink';
+colorBase1.className = 'color';
+colorBase2.className = 'color';
+colorBase3.className = 'color';
+colorBase4.className = 'color';
 
 // funções
 function clear() {
   for (let i = 0; i < listaDePixels.length; i += 1){
-    listaDePixels[i].style.backgroundColor = "white";
+    listaDePixels[i].style.backgroundColor = 'white';
   }
 }
 
 function colorSelect(n) {
   let color1 = false, color2 = false, color3 = false, color4 = false;
-
   if (n.target === colorBase1){
     color1 = true;
   } else if (n.target === colorBase2){
@@ -36,29 +39,16 @@ function colorSelect(n) {
   } else if (n.target === colorBase4){
     color4 = true
   };
-
   if (color1 === true){
-    colorBase1.className = "color selected";
-    colorBase2.className = "color";
-    colorBase3.className = "color";
-    colorBase4.className = "color";
+    colorBase1.className = 'color selected';
   } else if (color2 === true){
-    colorBase1.className = "color";
-    colorBase2.className = "color selected";
-    colorBase3.className = "color";
-    colorBase4.className = "color";
+    colorBase2.className = 'color selected';
   } else if (color3 === true){
-    colorBase1.className = "color";
-    colorBase2.className = "color";
     colorBase3.className = "color selected";
-    colorBase4.className = "color";
   } else if (color4 === true){
-    colorBase1.className = "color";
-    colorBase2.className = "color";
-    colorBase3.className = "color";
     colorBase4.className = "color selected";
   }
-}
+};
 
 function colorir(n) {
   if (colorBase1.className === "color selected") {
