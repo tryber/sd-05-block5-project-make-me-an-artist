1343e24b416f8a30d51f6bd48335a458d82d1d26
Larissa Palombo <larissampalombo@gmail.com>
2020-05-27 15:22:54 -0300

    mais js

--- script.js
+++ script.js
@@ -4,25 +4,34 @@ const cor2 = document.getElementById ('sqr2');
 const cor3 = document.getElementById ('sqr3');
 const cor4 = document.getElementById ('sqr4');
 let cor = "";
-const quadrado = document.getElementsByClassName ('.pixel');
+const quadrado = document.getElementById ('pixel-board');
 
 cor1.addEventListener("click", selecionaCor);
 cor2.addEventListener("click", selecionaCor);
 cor3.addEventListener("click", selecionaCor);
 cor4.addEventListener("click", selecionaCor);
 botao.addEventListener ("click", limpar);
+quadrado.addEventListener ("click", mudaCor);
 
 function selecionaCor (event) {
     if (event.target === cor1) {
+        cor.classList.remove ("selected");
+        cor1.classList.add ("selected");
         cor = event.target;
     }
     else if (event.target === cor2) {
+        cor.classList.remove("selected");
+        cor2.classList.add ("selected")
         cor = event.target;
     }
     else if (event.target === cor3) {
+        cor.classList.remove ("selected");
+        cor3.classList.add ("selected");
         cor = event.target;
     }
     else if (event.target === cor4) {
+        cor.classList.remove ("selected");
+        cor4.classList.add ("selected");
         cor = event.target;
     }
 }
