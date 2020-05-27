7a16fd6b96c91a1f2ed49f6e14ad3ea6f98c803c
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-27 18:47:02 -0300

    Consertei um erro de escrita no h1

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = function () {
     const quadroPixel = document.getElementById('pixel-board');
-    for (let l = 0; l < 5; l+=1) {
+    for (let l = 0; l < 5; l += 1) {
         const line = document.createElement('div');
         line.id = 'linha';
         quadroPixel.appendChild(line);
-        for (let i = 0; i < 5; i+=1) {
+        for (let i = 0; i < 5; i += 1) {
             const square = document.createElement('div');
             square.className = 'pixel fazLinha';
             line.appendChild(square);
