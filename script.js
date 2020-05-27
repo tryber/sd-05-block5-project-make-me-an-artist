1f959fbdd20cb8848b26f0295f29a260d8a8baf5
luialbeto <araujo.script@gmail.com>
2020-05-27 17:57:46 -0300

    subst script

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 //clicar em pixel dentro quadro após selecionar cor paleta, o pixel deve ser preenchido com esta cor//
-let colorPalette = document.querySelector
-let pixelBoard = document.querySelector
+let colorPalette = document.getElementById("color-pallete");//substitui query por element//
+let pixelBoard = document.getElementById("pixel-board");
+
+//classe selected adicionada à cor selecionada e removida da cor anteriormente selecionada//
+
