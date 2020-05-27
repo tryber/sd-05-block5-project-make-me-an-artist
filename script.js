26d4044a1dc48cb22c7bc439f8ead3f7902b8be5
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-27 18:12:27 -0300

    estrutura basica da página está pronta. Falta apenas a lógica para pintar no pixel board

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+    console.log("ate aqui ok");
+    let quadroPixel = document.getElementById("pixel-board");
+    for (let l = 0; l < 5; l++) {
+        let line = document.createElement("div");
+        line.id ="linha";
+        quadroPixel.appendChild(line);
+        for (let i = 0; i < 5; i++) {
+            console.log("ate aqui ok3");
+            let square = document.createElement("div");
+            square.className = "color fazLinha";
+            line.appendChild(square);
+        }
+    }
+}
