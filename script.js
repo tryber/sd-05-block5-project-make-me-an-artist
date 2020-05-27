62512d05a116df3616d2821787e1a7e8a6a52888
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-27 10:03:33 -0300

    Add funcao para colorir a paleta

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const first = document.getElementById('first');
 const second = document.getElementById('second');
 const third = document.getElementById('third');
 const colorPalette = document.getElementById('color-palette');
-const pixelQuadro = document.getElementById('pixel-board');
+const pixelBoard = document.getElementById('pixel-board');
 const clearBoard = document.getElementById('clear-board');
 let corAtual = black;
 
@@ -16,19 +16,22 @@ function selectColor() {
   corAtual = event.target;
 }
 
-// Altera a cor do pixel para a cor selecionada da paleta 
-function changeColor() {
-  
+// Colorir a paleta com a cor escolhida
+function colorPixel() {
+  let currentColor = corAtual.classList[1];
+  event.target.classList.remove(event.target.classList[1]);
+  event.target.classList.add(currentColor);
 }
 
 // Limpa a paleta de cores
 function clear() {
   let pixels = document.getElementsByClassName('pixel');
-  for (let i=0; i < pixels.length; i += 1) {
+  for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = 'white';
   }
 }
 
 // Event Listeners
-colorPalette.addEventListener('click' , selectColor);
+colorPalette.addEventListener('click', selectColor);
 clearBoard.addEventListener('click', clear);
+pixelBoard.addEventListener('click', colorPixel);
