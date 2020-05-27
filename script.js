0d36af1fb93d18a1fabcddc6c4f0084b08727aea
marinabrcls <barcelos.marina@gmail.com>
2020-05-27 18:30:28 -0300

    Refatorei a função de seleção

--- script.js
+++ script.js
@@ -6,39 +6,25 @@ let color3 = document.querySelector(".color-3");
 let colorPalette = document.getElementById("color-palette")
 let pixelBoard = document.getElementById("pixel-board");
 let botaoLimpar = document.getElementById("clear-board");
-let corSelecionada = black;
 
-//Funções
+// Funções
 
-//Essa é pra selecionar uma cor da paleta de cores
+// Essa é pra selecionar uma cor da paleta de cores
 function changeSelected() {
-  if (event.target === black) {
-    corSelecionada.classList.remove("selected");
-    black.classList.add("selected");
-    corSelecionada = event.target;
-  } else if (event.target === color1) {
-    corSelecionada.classList.remove("selected");
-    color1.classList.add("selected");
-    corSelecionada = event.target;
-  } else if (event.target === color2) {
-    corSelecionada.classList.remove("selected");
-    color2.classList.add("selected");
-    corSelecionada = event.target;
-  } else if (event.target === color3) {
-    corSelecionada.classList.remove("selected");
-    color3.classList.add("selected");
-    corSelecionada = event.target;
-  }
+  const corSelecionada = document.querySelector('.selected'); 
+  corSelecionada.classList.remove('selected');
+  event.target.classList.add('selected');
 }
 
-//Essa é pra colorir um pixal quando clicar
+// Essa é pra colorir um pixal quando clicar
 function colorPixel() {
+  corSelecionada = document.querySelector('.selected');
   let currentColor = corSelecionada.classList[1];
   event.target.classList.remove(event.target.classList[1]);
   event.target.classList.add(currentColor);
 }
 
-//Essa é pra fazer funcionar o botão limpar
+// Essa é pra fazer funcionar o botão limpar
 function limparTudo() {
   for (i = 0; i < pixelBoard.children.length; i++) {
     let colorClass = pixelBoard.children[i].classList[1];
@@ -47,7 +33,7 @@ function limparTudo() {
   }
 }
 
-//Event Listeners
+// Event Listeners
 colorPalette.addEventListener("click", changeSelected);
 pixelBoard.addEventListener("click", colorPixel);
 botaoLimpar.addEventListener("click", limparTudo);
