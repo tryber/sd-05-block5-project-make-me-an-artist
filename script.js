3129ccdc005910bbc6abeb600a2c079dd5d3a0b6
hleoc <informig@gmail.com>
2020-05-27 18:12:52 -0300

    Requisito 4 teste 12

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ window.onload = function () {
       let pixels = document.querySelectorAll('.pixel')
       for (let c = 0; c < pixels.length; c+=1) {
           pixels[c].style.backgroundColor = 'white';
-      }
-  }
+        }
+    }
   botaoMudaCor.addEventListener('click', trocaCorBranca);    
 }
-
-    
