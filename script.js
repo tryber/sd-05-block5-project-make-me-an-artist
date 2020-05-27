b447815e0774c23f7e1d948ce96048538fc16454
hleoc <informig@gmail.com>
2020-05-27 19:23:22 -0300

    Requisito 4 teste 21

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const botaoMudaCor = document.querySelector('#clear-board');
 function trocaCorBranca() {
     const pixels = document.querySelectorAll('.pixel');
     for (let c = 0; c < pixels.length; c+=1) {
-        pixels[c].style = "background-color: rgb(255, 255, 255);";
+        event.target.style = "background-color: rgb(255, 255, 255);";
       }
   }
 botaoMudaCor.addEventListener('click', trocaCorBranca);
