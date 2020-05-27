4a8ff60aca06f34bdceba4af9f044dcb4e06d761
hleoc <informig@gmail.com>
2020-05-27 17:38:00 -0300

    Requisito 4 teste 8

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = function () {
 
     //const caixaUm = document.querySelector('.color-black');
-    //const caixaDois = document.querySelector('color-red');
-    //const caixaTres = document.querySelector('color-blue');
-    //const caixaQuatro = document.querySelector('color-green');
+    //const caixaDois = document.querySelector('.color-red');
+    //const caixaTres = document.querySelector('.color-blue');
+    //const caixaQuatro = document.querySelector('.color-green');
     const botaoMudaCor = document.querySelector('#clear-board');
  
     function trocaCorBranca () {
