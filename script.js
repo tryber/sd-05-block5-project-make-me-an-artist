82635b9dcec4587c83a43a964664e30413780a31
Rafael Quinteiro Leite <rafaelqleite@gmail.com>
2020-05-27 20:21:37 -0300

    começo dos bonus

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 const selecionaPreto = document.getElementById('preto');
 let pegaCorSelecionada = getComputedStyle(selecionaPreto, null).getPropertyValue('background-color');
 const monitoraColorPalette = document.getElementById('color-palette');
-monitoraColorPalette.addEventListener('click', selecionaCorPaleta);
-//  monitorar "elements" nao deu certo - descobrir pq.
-//  let monitoraPixelBoard = document.getElementsByClassName('pixel');
-//  for (let i=0; i<monitoraPixelBoard.length; i+=1) {
-//  monitoraPixelBoard[i].addEventListener("click", encheCor(event));
-//  console.log(monitoraPixelBoard[i]);
-//  }
-const monitoriaLimpar = document.getElementById('clear-board');
-monitoriaLimpar.addEventListener('click', limpaTudo);
+const monitoraLimpar = document.getElementById('clear-board');
+const monitoraVQV = document.getElementById('generate-board');
 
 document.addEventListener('click', function (event) {
   if (event.target.classList.contains('pixel')) {
@@ -17,24 +10,43 @@ document.addEventListener('click', function (event) {
   }
 }, false);
 
-criaBoard(5);
-
 function criaBoard(n) {
   for (let i = 1; i <= n; i += 1) {
     const ultimaPosDentroBoard = document.getElementById('pixel-board');
     const criaDivLinha = document.createElement('div');
-    criaDivLinha.className = 'Linha' + i;
+    //criaDivLinha.className = 'Linha' + i;
     ultimaPosDentroBoard.appendChild(criaDivLinha);
 
     for (let j = 1; j <= n; j += 1) {
       const criaColuna = document.createElement('div');
       criaColuna.className = 'pixel';
-      criaColuna.className = 'pixel ' + 'Coluna' + j;
+      //criaColuna.className = 'pixel ' + 'Coluna' + j;
       const ultimaPosDentroLinha = ultimaPosDentroBoard.lastElementChild;
       ultimaPosDentroLinha.appendChild(criaColuna);
     }
   }
 }
+
+criaBoard(5);
+
+function usuarioCria() {
+  let conteudoInputSize = document.getElementById('board-size').value;
+  if (conteudoInputSize === ''){
+    alert('Board inválido!');
+  } else {
+    deletaTudo();
+    console.log(conteudoInputSize);
+    if (conteudoInputSize < 5) {
+      conteudoInputSize = 5;
+    } else if (conteudoInputSize > 50) {
+      conteudoInputSize = 50;
+    }
+    criaBoard(conteudoInputSize);
+  }
+}
+
+monitoraVQV.addEventListener('click', usuarioCria);
+
 function selecionaCorPaleta(event) {
   pegaCorSelecionada = getComputedStyle(event.target, null).getPropertyValue('background-color');
   if (event.target.className !== 'color selected') {
@@ -48,9 +60,18 @@ function selecionaCorPaleta(event) {
   }
 }
 
+monitoraColorPalette.addEventListener('click', selecionaCorPaleta);
+
 function limpaTudo() {
   const pegaPosicaoPixels = document.getElementsByClassName('pixel');
   for (let i = 0; i < pegaPosicaoPixels.length; i += 1) {
     pegaPosicaoPixels[i].style.backgroundColor = 'white';
   }
 }
+
+function deletaTudo() {
+  const pegaTudo = document.getElementById('pixel-board');
+  pegaTudo.innerHTML = "";
+}
+
+monitoraLimpar.addEventListener('click', limpaTudo);
