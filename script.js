67e2706568cfaa6549b2ce286dfeb05f1dbf914c
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-27 17:29:04 -0300

    Corrected grid

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function firstSelectedColor(){
+  let blackColor = document.getElementsByClassName("black");
+  blackColor[0].className = "selected";
+}
+
+// let selectedColor = document.getElementsByClassName("")
+// function selectColor(event){
+//   let color = event.target;
+//   if 
+// }
