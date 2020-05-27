65568c904037c6b7f1b34e6ad40bc4e96ede0c61
Larissa Palombo <larissampalombo@gmail.com>
2020-05-27 15:42:51 -0300

    corrigindo erros do console

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const cor2 = document.getElementById ('sqr2');
 const cor3 = document.getElementById ('sqr3');
 const cor4 = document.getElementById ('sqr4');
 let cor = black;
+let black = document.querySelector(".black");
 const quadrado = document.getElementById ('pixel-board');
 
 cor1.addEventListener("click", selecionaCor);
@@ -14,7 +15,7 @@ botao.addEventListener ("click", limpar);
 quadrado.addEventListener ("click", mudaCor);
 
 function selecionaCor (event) {
-    if (event.target === cor1) {
+    if (event.target === black) {
         cor.classList.remove ("selected");
         cor1.classList.add ("selected");
         cor = event.target;
