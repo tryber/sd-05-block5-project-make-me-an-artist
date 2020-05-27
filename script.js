dd1f6bd547bac788577181482c48dce7f2399e2f
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-27 18:29:01 -0300

    algumas alterações a pedido do code climate

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 window.onload = function () {
-    console.log("ate aqui ok");
-    let quadroPixel = document.getElementById("pixel-board");
-    for (let l = 0; l < 5; l++) {
-        let line = document.createElement("div");
-        line.id ="linha";
+    const quadroPixel = document.getElementById('pixel-board');
+    for (let l = 0; l < 5; l+=1) {
+        const line = document.createElement('div');
+        line.id = 'linha';
         quadroPixel.appendChild(line);
-        for (let i = 0; i < 5; i++) {
-            console.log("ate aqui ok3");
-            let square = document.createElement("div");
-            square.className = "pixel fazLinha";
+        for (let i = 0; i < 5; i+=1) {
+            const square = document.createElement('div');
+            square.className = 'pixel fazLinha';
             line.appendChild(square);
         }
     }
-}
+};
