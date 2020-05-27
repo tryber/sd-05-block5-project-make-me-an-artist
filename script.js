b151a06e08db8ae5104311305bc6ca08750a356c
hleoc <informig@gmail.com>
2020-05-27 11:41:32 -0300

    Requisito 4 teste 3

---
+++ script.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+
+const caixaUm = document.querySelector('.color-black');
+const caixaDois = document.querySelector('color-red');
+const caixaTres = document.querySelector('color-blue');
+const caixaQuatro = document.querySelector('color-green');
+
+function trocaCor () {
+
+}
+
+caixaUm.addEventListener('click', trocaCor);
+
+
+
+
+
+
+
+
+}
