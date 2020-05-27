b673471aade9eef761c15edd829474e3969d051c
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-27 09:40:47 -0300

    Funcao para limpar o quadro

--- script.js
+++ script.js
@@ -4,14 +4,31 @@ const first = document.getElementById('first');
 const second = document.getElementById('second');
 const third = document.getElementById('third');
 const colorPalette = document.getElementById('color-palette');
+const pixelQuadro = document.getElementById('pixel-board');
+const clearBoard = document.getElementById('clear-board');
 let corAtual = black;
 
 // Funções
+
 function selectColor() {
   corAtual.classList.remove('selected');
   event.target.classList.add('selected');
   corAtual = event.target;
 }
 
+// Altera a cor do pixel para a cor selecionada da paleta 
+function changeColor() {
+  
+}
+
+// Limpa a paleta de cores
+function clear() {
+  let pixel = document.getElementsByClassName("pixel");
+  for (let i=0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = "white";
+  }
+}
+
 // Event Listeners
 colorPalette.addEventListener('click' , selectColor);
+clearBoard.addEventListener("click", clear);
