711030806264a6adfe2a0ca8e25a6df8df86191b
hleoc <informig@gmail.com>
2020-05-27 19:14:24 -0300

    Requisito 4 teste 19

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-window.onload = function () {
-  const botaoMudaCor = document.querySelector('#clear-board');
-  function trocaCorBranca() {
-      const pixels = document.querySelectorAll('.pixel');
-      for (let c = 0; c < pixels.length; c+=1) {
-          pixels[c].style.backgroundColor = 'white';
-        }
-    }
-  botaoMudaCor.addEventListener('click', trocaCorBranca);
-};
+const botaoMudaCor = document.querySelector('#clear-board');
+function trocaCorBranca() {
+    const pixels = document.querySelectorAll('.pixel');
+    for (let c = 0; c < pixels.length; c+=1) {
+        pixels[c].style.backgroundColor = 'white';
+      }
+  }
+botaoMudaCor.addEventListener('click', trocaCorBranca);
