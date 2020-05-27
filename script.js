ee3057164d194995a7721cb409ebecd44ff8ff7c
Larissa Palombo <larissampalombo@gmail.com>
2020-05-27 15:03:42 -0300

    tentando pintar o quadrado

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const cores = document.getElementsByClassName ('color');
 const botao = document.getElementById ('clear-board');
 const cor1 = document.getElementById ('sqr1');
 const cor2 = document.getElementById ('sqr2');
 const cor3 = document.getElementById ('sqr3');
 const cor4 = document.getElementById ('sqr4');
-let sqr = "";
-const quadrado = document.getElementsByClassName ('pixel');
+let cor = "";
+const quadrado = document.getElementsByClassName ('.pixel');
 
 cor1.addEventListener("click", selecionaCor);
 cor2.addEventListener("click", selecionaCor);
@@ -15,21 +14,23 @@ botao.addEventListener ("click", limpar);
 
 function selecionaCor (event) {
     if (event.target === cor1) {
-        sqr = "black";
+        cor = event.target;
     }
     else if (event.target === cor2) {
-        sqr = "darkmagenta";
+        cor = event.target;
     }
     else if (event.target === cor3) {
-        sqr = "yellow";
+        cor = event.target;
     }
-    else if (event.target === cor4){
-        sqr = "lightseagreen";
+    else if (event.target === cor4) {
+        cor = event.target;
     }
 }
 
 function mudaCor () {
-    quadrado.style.getElementsByClassName = "background-color: sqr";
+    let novaCor = cor.classList[1];
+    event.target.classList.remove(event.target.classList[1]);
+    event.target.classList.add(novaCor);
 }
 
 function limpar (){
