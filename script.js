7017facb391188bb8f667fd34284ec58af5462a1
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-05-27 18:12:57 -0300

    code climate adjusted

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ let clearButton = document.querySelector('button');
 let palette = document.querySelector('#color-palette');
 let pixelBoard = document.querySelector('#pixel-board');
 let corSelecionada = document.querySelector('#selected');
-colorBase1.className = 'color selected';
 
 // definições prederminadas
 colorBase1.style.background = 'black';
 colorBase2.style.background = 'green';
 colorBase3.style.background = 'red';
 colorBase4.style.background = 'pink';
-colorBase1.className = 'color';
+colorBase1.className = 'color selected';
 colorBase2.className = 'color';
 colorBase3.className = 'color';
 colorBase4.className = 'color';
@@ -30,35 +29,35 @@ function clear() {
 
 function colorSelect(n) {
   let color1 = false, color2 = false, color3 = false, color4 = false;
-  if (n.target === colorBase1){
+  if (n.target === colorBase1) {
     color1 = true;
-  } else if (n.target === colorBase2){
+  } else if (n.target === colorBase2) {
     color2 = true;
-  } else if (n.target === colorBase3){
+  } else if (n.target === colorBase3) {
     color3 = true;
-  } else if (n.target === colorBase4){
-    color4 = true
+  } else if (n.target === colorBase4) {
+    color4 = true;
   };
-  if (color1 === true){
+  if (color1 === true) {
     colorBase1.className = 'color selected';
-  } else if (color2 === true){
+  } else if (color2 === true) {
     colorBase2.className = 'color selected';
-  } else if (color3 === true){
+  } else if (color3 === true) {
     colorBase3.className = "color selected";
-  } else if (color4 === true){
+  } else if (color4 === true) {
     colorBase4.className = "color selected";
   }
 };
 
 function colorir(n) {
-  if (colorBase1.className === "color selected") {
-    n.target.style.backgroundColor = "black";
-  } else if (colorBase2.className === "color selected") {
-    n.target.style.backgroundColor = "green";
-  } else if (colorBase3.className === "color selected") {
-    n.target.style.background = "red";
+  if (colorBase1.className === 'color selected') {
+    n.target.style.backgroundColor = 'black';
+  } else if (colorBase2.className === 'color selected') {
+    n.target.style.backgroundColor = 'green';
+  } else if (colorBase3.className === 'color selected') {
+    n.target.style.background = 'red';
   } else {
-    n.target.style.background = "pink";
+    n.target.style.background = 'pink';
   }
 }
 
