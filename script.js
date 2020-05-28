2c3b6574d2add32216d4c02031addc7e1f252dcd
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-27 22:33:38 -0300

    ..

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let blackColor = document.getElementsByClassName("black")[0];
-let redColor = document.getElementsByClassName("red")[0];
-let blueColor = document.getElementsByClassName("blue")[0];
-let greenColor = document.getElementsByClassName("green")[0];
+const blackColor = document.getElementsByClassName("black")[0];
+const redColor = document.getElementsByClassName("red")[0];
+const blueColor = document.getElementsByClassName("blue")[0];
+const greenColor = document.getElementsByClassName("green")[0];
 let selectedColor = document.getElementsByClassName("selected")[0];
 
 blackColor.className += " selected";
@@ -11,3 +11,20 @@ redColor.addEventListener("click", selectColor);
 blueColor.addEventListener("click", selectColor);
 greenColor.addEventListener("click", selectColor);
 
+function selectColor(event){
+  let chosenColor = event.target;
+  if (chosenColor === blackColor){
+    selectedColor.classList.remove("selected");
+    blackColor.classList.add("selected");
+  } else if (chosenColor === redColor){
+    selectedColor.classList.remove("selected");
+    redColor.classList.add("selected");
+  } else if (chosenColor === blueColor){
+    selectedColor.classList.remove("selected");
+    blueColor.classList.add("selected");
+  } else if (chosenColor === greenColor){
+    selectedColor.classList.remove("selected");
+    greenColor.classList.add("selected");
+  }
+  selectedColor = document.getElementsByClassName("selected")[0];
+} 
