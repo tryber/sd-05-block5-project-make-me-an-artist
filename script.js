35745a94dd4950628498f66d4e74d96ec0a889eb
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-28 02:43:25 -0300

    tentando agradar o codeclimate

--- script.js
+++ script.js
@@ -1,58 +1,55 @@
 window.onload = function () {
-    let id = 0;
-    const quadroPixel = document.getElementById('pixel-board');
-    for (let l = 0; l < 5; l += 1) {
-        const line = document.createElement('div');
-        line.className = 'linha largura';
-        quadroPixel.appendChild(line);
-        
-        for (let i = 0; i < 5; i += 1) {
-            const square = document.createElement('div');
-            square.className = 'pixel fazLinha branco';
-            square.id = (id);
-            line.appendChild(square);
-            id += 1;
-        }
+  let id = 0;
+  const quadroPixel = document.getElementById("pixel-board");
+  for (let l = 0; l < 5; l += 1) {
+    const line = document.createElement("div");
+    line.className = "linha largura";
+    quadroPixel.appendChild(line);
+    for (let i = 0; i < 5; i += 1) {
+      const square = document.createElement("div");
+      square.className = "pixel fazLinha branco";
+      square.id = id;
+      line.appendChild(square);
+      id += 1;
     }
+  }
+};
+const paleta = document.getElementById("color-palette");
+const div = paleta.children;
+const pixelBoard = document.getElementById("pixel-board");
+let cor = ["preto"];
 
-    const paleta = document.getElementById('color-palette');
-    const div = paleta.children;
-    const pixelBoard = document.getElementById('pixel-board');
-    let cor = ['preto'];
+paleta.addEventListener("click", mudaCorSelecionada);
 
-    paleta.addEventListener('click', mudaCorSelecionada);
-
-    function mudaCorSelecionada(evento) {
-        for (let i = 0; i < paleta.childElementCount; i += 1) {
-            div[i].classList.remove('selected');
-        }
-        cor = [];
-        const cornova = evento.target;
-        const bloco = cornova.classList.value;
-        const lista = bloco.split(' ');
-        cor.push(lista[lista.length - 1]);
-        cornova.classList.add('selected');
-    }
-
-    pixelBoard.addEventListener('click', pincel) 
-
-    function pincel(event) {
-        let contato = event.target;
-        let listaString = contato.classList.value;
-        let listaItem = listaString.split(' ');
-        contato.classList.remove(listaItem[2]);
-        contato.classList.add(cor);
-    }
-
-    const botaoLimpar = document.getElementById('clear-board');
-    botaoLimpar.addEventListener('click', function() {
-        for (let i = 0; i < id; i += 1) {
-            let str = i.toString();
-            let square = document.getElementById(str);
-            let squar = square.classList.value;
-            let resetList = squar.split(' ');
-            square.classList.remove(resetList[2]);
-            square.classList.add('branco');
-        }
-    })
+function mudaCorSelecionada(evento) {
+  for (let i = 0; i < paleta.childElementCount; i += 1) {
+    div[i].classList.remove("selected");
+  }
+  cor = [];
+  const cornova = evento.target;
+  const bloco = cornova.classList.value;
+  const lista = bloco.split(" ");
+  cor.push(lista[lista.length - 1]);
+  cornova.classList.add("selected");
 }
+
+function pincel(event) {
+  const contato = event.target;
+  const listaString = contato.classList.value;
+  let listaItem = listaString.split(" ");
+  contato.classList.remove(listaItem[2]);
+  contato.classList.add(cor);
+}
+
+pixelBoard.addEventListener("click", pincel);
+const botaoLimpar = document.getElementById("clear-board");
+botaoLimpar.addEventListener("click", function () {
+  for (let i = 0; i < id; i += 1) {
+    const str = i.toString();
+    const square = document.getElementById(str);
+    const squar = square.classList.value;
+    const resetList = squar.split(" ");
+    square.classList.remove(resetList[2]);
+    square.classList.add("branco");
+  }
+});
