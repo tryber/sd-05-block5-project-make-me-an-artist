6abe24070efa8a312fe8d86634b78b47f32b1183
hleoc <informig@gmail.com>
2020-05-28 12:59:58 -0300

    Requisito final

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const botaoMudaCor = document.querySelector('#clear-board');
-function trocaCorBranca() {
-    const pixels = document.querySelectorAll('.pixel');
-    for (let c = 0; c < pixels.length; c+=1) {
-        event.target.style = "background-color: rgb(255, 255, 255);";
+window.onload = function () {
+
+  const botaoMudaCor = document.querySelector('#clear-board');
+  const caixaUm = document.querySelector('.color-black');
+  const caixaDois = document.querySelector('.color-red');
+  const caixaTres = document.querySelector('.color-blue');
+  const caixaQuatro = document.querySelector('.color-green');
+   
+  function trocaCorBranca() {
+      const pixels = document.querySelectorAll('.pixel');
+      for (let c = 0; c < pixels.length; c+=1) {
+          pixels[c].style.backgroundColor = 'white';
+        }
+    }
+  botaoMudaCor.addEventListener('click', trocaCorBranca);
+
+  function trocaCor(event) {
+      const cores = document.querySelectorAll('.pixel');
+      for (let x = 0; c < pixels.length; c+=1) {
+
       }
-  }
-botaoMudaCor.addEventListener('click', trocaCorBranca);
+    }
+
+  caixaUm.addEventListener('click', trocaCor);
+
+};
