ac0bc3ed1ac2776c11cab6fece614732925d23f0
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-27 22:10:01 -0300

    adicionei a classe selected ao preto.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function () {
     const quadroPixel = document.getElementById('pixel-board');
     for (let l = 0; l < 5; l += 1) {
         const line = document.createElement('div');
-        line.id = 'linha';
+        line.className = 'linha largura';
         quadroPixel.appendChild(line);
         for (let i = 0; i < 5; i += 1) {
             const square = document.createElement('div');
@@ -11,3 +11,11 @@ window.onload = function () {
         }
     }
 };
+
+const paleta = document.getElementById('color-palette');
+paleta.addEventListener("click", mudaCorSelecionada)
+
+function mudaCorSelecionada (evento) {
+    let cor = evento.target;
+    
+}
