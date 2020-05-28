af3cdf57eb8a0a0eddbe1fe5b6df8912a6573846
luizf.avelar <luiz.avelar25@gmail.com>
2020-05-27 21:40:39 -0300

    ..

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let blackColor = document.getElementsByClassName("black")[0];
 let redColor = document.getElementsByClassName("red")[0];
 let blueColor = document.getElementsByClassName("blue")[0];
 let greenColor = document.getElementsByClassName("green")[0];
+let selectedColor = document.getElementsByClassName("selected")[0];
 
 blackColor.className += " selected";
 
@@ -9,15 +10,4 @@ blackColor.addEventListener("click", selectColor);
 redColor.addEventListener("click", selectColor);
 blueColor.addEventListener("click", selectColor);
 greenColor.addEventListener("click", selectColor);
-
-let selectedColor = document.getElementsByClassName("selected")[0];
-
-function selectColor (event){
-  color = event.target;
-  if (selectedColor !== color){
-    selectedColor.classList.add = "selected";
-    color.classList.remove = "selected";
-    selectedColor = document.getElementsByClassName("selected")[0];
-  }
-}
 
