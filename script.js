40bff241881c8558765d048dc3e920be375395d0
luialbeto <araujo.script@gmail.com>
2020-05-28 11:20:06 -0300

    function

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function Color() {
   }
 } //função seletora//
 
-paletteColor.addEventListener('click', function () {
+paletteColor.addEventListener('click', function ()) {
   const selectedColor = document.querySelector('.selected');
   selectedColor.classList.remove('selected');
   event.target.classList.add('selected');
