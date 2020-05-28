67fdd0ac4f7aa1c7c7ac7bc9177822c1754a5530
luialbeto <araujo.script@gmail.com>
2020-05-28 12:54:03 -0300

    function

--- script.js
+++ script.js
@@ -18,15 +18,15 @@ function Color() {
   for (let i = 0; i < n; i += 1) {
     const color2 = document.createElement('div');
     colorPalette.appendChild(color2);
-    color.classList.add('secondcolor');
-    color.style.backgroundColor = randomColor();
+    color2.classList.add('secondcolor');
+    color2.style.backgroundColor = randomColor();
   }
 } //função seletora//
 for (c of color2) {
 c.addEventListener('click', function (event) {
 for (s of color2)  {
-    s.classList.remove('selected')
+    s.classList.remove('selected');
 }
-event.target.classList.add('selected')
+event.target.classList.add('selected');
 })
 }
