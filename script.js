b7e33122bf4f93e0d46a2a733c2a1699e9a1d0d1
luialbeto <araujo.script@gmail.com>
2020-05-28 10:24:33 -0300

    function

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //clicar em pixel dentro quadro... preenchido com esta cor//
-let colorPalette = document.getElementById("color-pallete");  //substitui query por element//
-let pixelBoard = document.getElementById("pixel-board");
+const colorPalette = document.getElementById('color-pallete');  //substitui query por element//
+const pixelBoard = document.getElementById('pixel-board');
 //classe selected ...e removida da cor anteriormente selecionada//
 
-let select = document.getElementsByClassName("selected")[0];
-let color = select.classList[1];
-let target = "";
+const select = document.getElementsByClassName('selected')[0];
+const color = select.classList[1];
+const target = '';
 
-function setColor(n) {
+function Color(n) {
   paletteColor.innerHTML = '';
-  let preto = document.createElement('div');
+  const black = document.createElement('div');
   paletteColor.appendChild(black);
-  black.className = 'color selected';
+  black.className = 'selected';
   black.style.backgroundColor = 'black';
   for (let param = 0; param < n; param += 1) {
-    let cor = document.createElement('div');
+    const cor = document.createElement('div');
     paletteColor.appendChild(color);
     color.classList.add('secondcolor');
     color.style.backgroundColor = randomColor();
