adc867a51557cc78b8431a4459e9936ad331aa97
fabiano Alves <fasnaweb2020@gmail.com>
2020-05-28 00:21:30 -0300

    Requisito  Criar um botão limpa quadro cocluido

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-
-
+//Essa parte do código preenche o de branco  fundo de todos os quadros
+function PintarFundo(){
+    const tdQuadroPixel = document.getElementsByClassName('pixel');
+      for(let i = 0;i < tdQuadroPixel.length;i +=1){
+        tdQuadroPixel[i].style.backgroundColor='white';
+      }
+}
+let limparQuadros = document.getElementById('clear-board');
+    limparQuadros.addEventListener('click',PintarFundo);
+//fim do código ...
