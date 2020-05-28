51f9a3bd8e3e871cd5705f2a610999c7306ffc61
luialbeto <araujo.script@gmail.com>
2020-05-28 08:24:53 -0300

    function

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let colorPalette = document.getElementById("color-pallete");//substitui query po
 let pixelBoard = document.getElementById("pixel-board");
 //classe selected adicionada à cor selecionada e removida da cor anteriormente selecionada//
 let select = document.getElementsByClassName("selected")[0];
-let changeColor = selected.classList[1]
-function change (event) {
+let changeColor = selected.classList[1];
+let target = "";
+target.classList.add("selected");
+function change(event) {
 target = event.target;
-selected.classList.remove('selected');
-target.classList.add('selected');
-selected = document.getElementsByClassName('selected')[0];
+selected.classList.remove("selected");
+target.classList.add("selected");
+selected = document.getElementsByClassName("selected")[0];
 }//função seletora//
