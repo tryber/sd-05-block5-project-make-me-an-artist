d947776add2a2030a8028fc89757c5f08721a03f
luialbeto <araujo.script@gmail.com>
2020-05-28 10:01:17 -0300

    function

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-//clicar em pixel dentro quadro após selecionar cor paleta, o pixel deve ser preenchido com esta cor//
-let colorPalette = document.getElementById("color-pallete");//substitui query por element//
+//clicar em pixel dentro quadro... preenchido com esta cor//
+let colorPalette = document.getElementById("color-pallete");  //substitui query por element//
 let pixelBoard = document.getElementById("pixel-board");
-//classe selected adicionada à cor selecionada e removida da cor anteriormente selecionada//
+//classe selected ...e removida da cor anteriormente selecionada//
 
 let select = document.getElementsByClassName("selected")[0];
 let color = select.classList[1];
 let target = "";
 
-function change() {
-target = event.target;
-selected.classList.remove("selected");
-target.classList.add('selected');
-selected = document.getElementsByClassName('selected')[0];
-}
+function setColor(n) {
+  paletteColor.innerHTML = '';
+  let preto = document.createElement('div');
+  paletteColor.appendChild(black);
+  black.className = 'color selected';
+  black.style.backgroundColor = 'black';
+  for (let param = 0; param < n; param += 1) {
+    let cor = document.createElement('div');
+    paletteColor.appendChild(color);
+    color.classList.add('secondcolor');
+    color.style.backgroundColor = randomColor();
+  }
+} //função seletora//
 
-function Pixel() {
-color = selected.classList[1];
-target = event.target;
-let firstColor = target.classList[target.classList.length - 1];
-target.classList.remove(firstColor);
-target.classList.remove(firstColor);
-let secondColor = "background-color: ${cor} !important";
-target.classList.add(color);
-target.style = secondColor;
-}//função seletora//
+paletteColor.addEventListener('click', function () {
+  const selectedColor = document.querySelector('.selected');
+  selectedColor.classList.remove('selected');
+  event.target.classList.add('selected');
+}
