fe042eb3dc9a7d689b6db7451de146004a448483
SamuelPPlab <samuelpp.s@hotmail.com>
2020-05-28 01:47:40 -0300

    a classe já está sendo adicionada ao lugar que foi clicado.

--- script.js
+++ script.js
@@ -12,17 +12,30 @@ window.onload = function () {
     }
 };
 
-const paleta = document.getElementById('color-palette');
 
-paleta.addEventListener('click', mudaCorSelecionada); 
+const paleta = document.getElementById('color-palette');
+const div = paleta.children;
+const pixelBoard = document.getElementById('pixel-board');
+let cor = ['black'];
+
+paleta.addEventListener('click', mudaCorSelecionada);
 
 function mudaCorSelecionada(evento) {
     for (let i = 0; i < paleta.childElementCount; i += 1) {
-        let div = paleta.children[i];
-        div.classList.remove('selected');
+        div[i].classList.remove('selected');
     }
-    let cornova = evento.target;
+    cor = [];
+    const cornova = evento.target;
+    const bloco = cornova.classList.value;
+    const lista = bloco.split(' ');
+    cor.push(lista[lista.length - 1]);
     cornova.classList.add('selected');
 }
 
+pixelBoard.addEventListener('click', pincel) 
 
+function pincel(event) {
+    let contato = event.target;
+    let coordenada = contato.classList;
+    coordenada.add(cor)
+}
