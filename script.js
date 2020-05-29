5db58b7625cccfd60b8ab0ba603cd0bd8b2d5e16
hleoc <informig@gmail.com>
2020-05-28 21:39:38 -0300

    Requisito final teste 4

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 window.onload = function () {
-
   const botaoMudaCor = document.querySelector('#clear-board');
   const cores = document.querySelectorAll('.color');
+  const mudaCor = document.querySelector('#pixel-board');
+  //const umQuadrado = document.querySelector('#um');
   
-   
+  function coresPixels() {
+    let selected = document.querySelector('.selected');
+    let pixelClicado = selected.classList[1];
+    let corRemovida = event.target.classList[1];
+    event.target.classList.remove(corRemovida);
+    event.target.classList.add(pixelClicado);
+  }
+  mudaCor.addEventListener('click', coresPixels);
+  
+
   function trocaCorBranca() {
       const pixels = document.querySelectorAll('.pixel');
       for (let c = 0; c < pixels.length; c+=1) {
           pixels[c].style.backgroundColor = 'white';
-        }
-    }
+      }
+  }
   botaoMudaCor.addEventListener('click', trocaCorBranca);
 
   function trocaCor(event) {
