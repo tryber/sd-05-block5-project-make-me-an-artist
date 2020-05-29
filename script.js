af6ac281d9613b0c6348568e6e58af7e879975da
Lucas Costa Ventura <lucascv333@gmail.com>
2020-05-28 21:38:14 -0300

    alterando funcao createPixels

--- script.js
+++ script.js
@@ -35,17 +35,22 @@ colorir.addEventListener('click', function (event) {
 
 // Função do botão que cria Pixels
 function createPixels() {
-  if (document.querySelector('#board-size').value === '') {
+  const quantidadePixels = document.querySelector('#board-size').value;
+  if (quantidadePixels === '') {
     alert('Board inválido!');
   }
   else {
-    for (let i = 0; i < document.querySelector('#board-size').value; i += 1) {
+    while (document.querySelectorAll('.linha').length) {
+      const itemRemovido = document.querySelectorAll('.linha')[0];
+      document.querySelector('#pixel-board').removeChild(itemRemovido);
+    }
+    for (let i = 0; i < quantidadePixels; i += 1) {
       const linha = document.createElement('tr');
       linha.className = 'linha';
       document.querySelector('#pixel-board').appendChild(linha);
     }
     for (let i = 0; i < document.querySelectorAll('.linha').length; i += 1) {
-      for (let n = 0; n < document.querySelector('#board-size').value; n += 1) {
+      for (let n = 0; n < quantidadePixels; n += 1) {
         const novoPixel = document.createElement('td');
         novoPixel.className = 'pixel';
         document.querySelectorAll('.linha')[i].appendChild(novoPixel);
