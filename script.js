2e58a821c4a99a81286bcc9818cc0d7754f45f00
luialbeto <araujo.script@gmail.com>
2020-05-31 18:04:03 -0300

    cores

--- script.js
+++ script.js
@@ -29,29 +29,3 @@ for (let k = 0; k < paletaDeCores.length; k+=1) {
 for (let l = 0; l < quadrados.length; l+=1) {
 quadrados[l].addEventListener('click', colocarCor);
 }
-
-/*function trocaCorBranca() {
-  const quadrados = document.querySelectorAll('.pixel-board');
-    for (let i = 0; i < quadrados.length; i+=1) {
-  quadrados[i].style.backgroundColor = 'white';
-}
-}
-
-botaoMudar.addEventListener('click', trocaCorBranca);
-  let pixels = document.querySelectorAll('.pixel');
-    for (let m = 0; m < pixels.length-1; m+=1) {
-      pixels[m].style.backgroundColor = 'white'
-    }*/
-   /* function botaoParaLimparCores() {
-    let pixels = document.getElementsByClassName("pixel");
-    for(let i = 0; i < pixels.length; i+=1) {
-    let temp = document.createElement("td");
-    temp.className = "pixel";
-    temp.style.backgroundColor = "white";
-    console.log(document.querySelectorAll("td.pixel")[i] = temp);
-    document.querySelectorAll("td.pixel")[i];
-    }
-    let board = document.getElementsByClassName("pixel");
-    board.innerHTML = botao.replaceWith.white
-    }
-    botaoLimpar.addEventListener('click', botaoLimpar);*/
