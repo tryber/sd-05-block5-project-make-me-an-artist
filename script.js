00da35ff33de7f1689adc7c982d7a17e96cd6bfe
luialbeto <araujo.script@gmail.com>
2020-05-31 17:42:36 -0300

    corespixel

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 //variáveis declaradas//
 const paletaDeCores = document.querySelectorAll('.color');
 const corSelecionada = document.getElementById('selected');
-const quadrados = document.querySelector('.pixel');
+const quadrados = document.querySelectorAll('.pixel');
 const corBranca = document.getElementById('white');
 const botaoLimpar = document.getElementById('clear-board');
-const coresAtuais = ['c1', 'c2','c3', 'c4'];
+const coresAtuais = document.getElementById('#color-palete');
   //funções//
 function selecionaCor (event) {
   console.log(event.target);
@@ -16,7 +16,7 @@ function selecionaCor (event) {
   }
 function colocarCor (event) {
   let selecionaDiv = document.querySelector('.selected');
-    event.target.style.backgroundColor = getComputedStyle(selecionaDiv).backgroundColor;
+    event.target.style.backgroundColor = selecionaDiv.id;
 }
 function removeSelected () {
   for (let j = 0; j < paletaDeCores.length; j+=1) {
@@ -27,12 +27,31 @@ for (let k = 0; k < paletaDeCores.length; k+=1) {
   paletaDeCores[k].addEventListener('click', selecionaCor);
 }
 for (let l = 0; l < quadrados.length; l+=1) {
-quadrados[l].addEventlistener('click', colocarCor);
+quadrados[l].addEventListener('click', colocarCor);
 }
 
-function trocaCorBranca() {
+/*function trocaCorBranca() {
   const quadrados = document.querySelectorAll('.pixel-board');
     for (let i = 0; i < quadrados.length; i+=1) {
   quadrados[i].style.backgroundColor = 'white';
 }
 }
+
+botaoMudar.addEventListener('click', trocaCorBranca);
+  let pixels = document.querySelectorAll('.pixel');
+    for (let m = 0; m < pixels.length-1; m+=1) {
+      pixels[m].style.backgroundColor = 'white'
+    }*/
+   /* function botaoParaLimparCores() {
+    let pixels = document.getElementsByClassName("pixel");
+    for(let i = 0; i < pixels.length; i+=1) {
+    let temp = document.createElement("td");
+    temp.className = "pixel";
+    temp.style.backgroundColor = "white";
+    console.log(document.querySelectorAll("td.pixel")[i] = temp);
+    document.querySelectorAll("td.pixel")[i];
+    }
+    let board = document.getElementsByClassName("pixel");
+    board.innerHTML = botao.replaceWith.white
+    }
+    botaoLimpar.addEventListener('click', botaoLimpar);*/
