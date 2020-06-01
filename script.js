c3f038140b4b583dac6a9ad0f86c3f1cb6bb30e2
Mau <mauricio.sdjr@gmail.com>
2020-06-01 12:05:35 -0300

    codeclimate

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-//Create pixel board
-let pixelBoard = document.getElementById("pixel-board");
-let tagTR = document.getElementsByTagName("tr");
-let numberTagTR;
-const genBoardButton = document.getElementById("generate-board");
-const numberOfPixels = document.getElementById("board-size");
+// Create pixel board
+const pixelBoard = document.getElementById('pixel-board');
+const tagTR = document.getElementsByTagName('tr');
+const numberTagTR;
+const genBoardButton = document.getElementById('generate-board');
+const numberOfPixels = document.getElementById('board-size');
 
 function boardInit(pixelNumbers) {
   for (let i = 0; i < pixelNumbers; i += 1) {
-    let pixelLine = document.createElement("tr");
+    let pixelLine = document.createElement('tr');
     pixelBoard.appendChild(pixelLine);
   }
   for (let i = 0; i < pixelNumbers; i += 1) {
     for (let j = 0; j < tagTR.length; j += 1) {
-      let pixelColumn = document.createElement("td");
+      let pixelColumn = document.createElement('td');
       numberTagTR = tagTR[j];
       numberTagTR.appendChild(pixelColumn);
-      pixelColumn.className = "pixel white";
-      pixelColumn.addEventListener("click", paintPixel);
+      pixelColumn.className = 'pixel white';
+      pixelColumn.addEventListener('click', paintPixel);
     }
   }
 }
 boardInit(5);
 
 function createBoardSize() {
-  if (numberOfPixels.value === "") {
-    alert("Board inválido!");
+  if (numberOfPixels.value === '') {
+    alert('Board inválido!');
   }
   if (numberOfPixels.value < 5) {
     numberOfPixels.value = 5;
@@ -33,70 +33,70 @@ function createBoardSize() {
     numberOfPixels.value = 50;
   }
   if (numberOfPixels.value >= 5 && numberOfPixels.value <= 50) {
-    pixelBoard.innerText = "";
+    pixelBoard.innerText = '';
     for (let i = 0; i < numberOfPixels.value; i += 1) {
-      let pixelLine = document.createElement("tr");
+      let pixelLine = document.createElement('tr');
       pixelBoard.appendChild(pixelLine);
     }
     for (let i = 0; i < numberOfPixels.value; i += 1) {
       for (let j = 0; j < tagTR.length; j += 1) {
-        let pixelColumn = document.createElement("td");
+        let pixelColumn = document.createElement('td');
         numberTagTR = tagTR[j];
         numberTagTR.appendChild(pixelColumn);
-        pixelColumn.className = "pixel white";
-        pixelColumn.addEventListener("click", paintPixel);
+        pixelColumn.className = 'pixel white';
+        pixelColumn.addEventListener('click', paintPixel);
       }
     }
   }
 }
 
-genBoardButton.addEventListener("click", createBoardSize);
+genBoardButton.addEventListener('click', createBoardSize);
 
-//Change class "selected"
-let colorBlack = document.getElementById("cor1");
-let colorGreen = document.getElementById("cor2");
-let colorYellow = document.getElementById("cor3");
-let colorRed = document.getElementById("cor4");
-let colorSelected = document.getElementsByClassName("selected")[0];
-let colorPalette = document.getElementById("color-palette");
+// Change class 'selected'
+const colorBlack = document.getElementById('cor1');
+const colorGreen = document.getElementById('cor2');
+const colorYellow = document.getElementById('cor3');
+const colorRed = document.getElementById('cor4');
+const colorSelected = document.getElementsByClassName('selected')[0];
+const colorPalette = document.getElementById('color-palette');
 
 function changeSelected() {
   if (event.target === colorBlack) {
-    colorSelected.classList.remove("selected");
-    colorBlack.classList.add("selected");
+    colorSelected.classList.remove('selected');
+    colorBlack.classList.add('selected');
     colorSelected = event.target;
   } else if (event.target === colorGreen) {
-    colorSelected.classList.remove("selected");
-    colorGreen.classList.add("selected");
+    colorSelected.classList.remove('selected');
+    colorGreen.classList.add('selected');
     colorSelected = event.target;
   } else if (event.target === colorYellow) {
-    colorSelected.classList.remove("selected");
-    colorYellow.classList.add("selected");
+    colorSelected.classList.remove('selected');
+    colorYellow.classList.add('selected');
     colorSelected = event.target;
   } else if (event.target === colorRed) {
-    colorSelected.classList.remove("selected");
-    colorRed.classList.add("selected");
+    colorSelected.classList.remove('selected');
+    colorRed.classList.add('selected');
     colorSelected = event.target;
   }
 }
 
-colorPalette.addEventListener("click", changeSelected);
+colorPalette.addEventListener('click', changeSelected);
 
-//Paint pixels
+// Paint pixels
 function paintPixel() {
   let colorClass = colorSelected.classList[1];
   event.target.classList.remove(event.target.classList[1]);
   event.target.classList.add(colorClass);
 }
 
-//Clear Board
+// Clear Board
 function clearBoard() {
-  let allPixels = document.querySelectorAll("#pixel-board > tr > td");
+  let allPixels = document.querySelectorAll('#pixel-board > tr > td');
   for (let i = 0; i < allPixels.length; i += 1) {
     allPixels[i].classList.remove(allPixels[i].classList[1]);
-    allPixels[i].classList.add((allPixels[i].classList[1] = "white"));
+    allPixels[i].classList.add((allPixels[i].classList[1] = 'white'));
   }
 }
 
-let clearButton = document.getElementById("clear-board");
-clearButton.addEventListener("click", clearBoard);
+const clearButton = document.getElementById('clear-board');
+clearButton.addEventListener('click', clearBoard);
