8fbce793ecab41e793851ecde4febe0da77070ca
Thiago <asm.thiago@gmail.com>
2020-06-01 19:15:50 -0300

    Pintar QUadrado

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-let paleta =  document.getElementById('color-palette');
+let paleta = document.getElementById('color-palette');
 
-paleta.addEventListener('click', function(duKaralho){
+paleta.addEventListener('click', function (duKaralho) {
   if (duKaralho.target.classList[0] == 'color') {
-  let corAntiga = document.getElementsByClassName('selected')[0];
-  console.log(corAntiga.classList.toggle('selected'));
-  let corEscolhida = duKaralho.target;
-  corEscolhida.classList.toggle('selected');
+    let corAntiga = document.getElementsByClassName('selected')[0];
+    corAntiga.classList.toggle('selected');
+    let corEscolhida = duKaralho.target;
+    corEscolhida.classList.toggle('selected');
   }
 });
 
+let pixelBoard = document.getElementById('pixel-board');
+
+pixelBoard.addEventListener('click', function (doiDimais) {
+  if (doiDimais.target.classList[0] == "pixel") {
+    let pixelEscolhido = doiDimais.target;
+    let corEscolhida = document.getElementsByClassName('selected')[0];
+    let propriedadeCss = window.getComputedStyle(corEscolhida, null).getPropertyValue("background-color");
+    pixelEscolhido.style.backgroundColor = propriedadeCss;
+    console.log(pixelEscolhido.style.backgroundColor);
+  }
+})
