d45f1a81d084d1bff3fbfd3c96acbb0c921e74fd
preto115 <preto115@gmail.com>
2020-06-03 01:18:47 -0300

    implementando funcao para habilitar o usuario a inserir a quantidade de pixel

--- script.js
+++ script.js
@@ -6,67 +6,75 @@
     let pixelBoard = document.querySelector("#pixel-board");
     let pixel = document.querySelectorAll(".pixel");
     document.querySelector(".black").classList.add("selected");
-    let boardSize = document.getElementById("board-size").value;
+    let boardSize = document.getElementById("board-size");
     const botaoGeraQuadro = document.getElementById("generate-board");
 
-    boardSize = '';
     let corEscolhida = 'black';
     let tamanhoDoBoard = 0;
     let tamanho= '';
    
-    window.alert("Ola! para pintar o sete insira o tamanho do quadro");    
-
-   function configuraBoard(b){
-    let board ='';
-        for(let c=0; c < b; c +=1){
-            let divLine = document.createElement("div");
-            divLine[c].className = "lines";
-            for(let cont=c; cont < b; cont +=1){
-                let divPixel = document.createElement("div");
-                divPixel[cont].className = "pixel";
-                document.getSelection(divLine[c])[0].appendChild(divPixel[cont]);
-            }
-            document.pixelBoard[0].appendChild(divLine[c]);
-            board.push(divLine[c]);
-        }
-        return board;
-    } 
-function checaEntrada(a){ 
-    a = Number(boardSize);
-    let valor = 0;
-if(boardSize != null){
-
-  // if(boardSize == 0){
-  //alert("Board inválido!");
- //  }
-   if(a < 5){
-       valor = 5;
-   }
-   else if(a > 50){
-        valor = 50;
-   }
-   else {
-        valor = a;
-   }       
-    return valor;
-}
-    else prompt("Preencha o valor e clique em \'VQV:\'") 
-}
-    function limpaPixel(){
-        for(apaga in pixel){
-        pixel[apaga].style = "background-color: white";
-        }
+   function configuraBoard(a){
+    let board ='', valor= '';
+    if(a < 5){
+        valor = 5;
     }
+    else if(a > 50){
+         valor = 50;
+    }
+    else {
+         valor = a;
+    } 
+    pixelBoard.innerHTML = "";
+        for(let c = 0; c < valor; c +=1){
+            let linha = document.createElement("div");
+            linha.className = "lines";
+            for(let cont = 0; cont < valor; cont +=1){
+                let pixelCell = document.createElement("div");
+                pixelCell.className = "pixel borda";
+                pixelCell.style.backgroundColor = "white";
+               // console.log("pixel: ", pixelCell.innerText);
+                linha.appendChild(pixelCell);
+            }
+             pixelBoard.appendChild(linha);
+            
+        }
+      //  return board;
+    }
+botaoGeraQuadro.addEventListener('click', checaEntrada);
+function checaEntrada(){ 
+    let valor = boardSize.value;
+if (valor != ""){
+
+   if(valor == 0){
+  alert("Tamanho inválido! Digite um valor diferente de 0");
+    boardSize.value = '';
+    boardSize.focus();
+  }
+        
+   else configuraBoard(valor);
+}
+    else { alert("Preencha o valor e clique em \'VQV:\'")} 
+    boardSize.value = '';
+    boardSize.focus();
+
+}   
+clearButton.addEventListener('click', limpaPixel);
+    function limpaPixel(){
+        for(item in pixel){
+        pixel[item].style = "background-color = white;";
+        }
+   }
     function selecionaCor(e){
         corEscolhida = e.target;
         for(i = 0; i < selectedColor.length; i+=1){ 
-            selectedColor[i].classList.remove("selected");
 
             if(  selectedColor[i] == corEscolhida){
                 corEscolhida.classList.add("selected");
                 document.querySelector(".selecao").textContent = e.target.classList[0];
             }
-            else {            
+            else {
+                    selectedColor[i].classList.remove("selected");
+         
         }
         }
          corEscolhida = getComputedStyle(corEscolhida, null).getPropertyValue('background-color');
@@ -75,11 +83,12 @@ if(boardSize != null){
         let pixelClicado = e.target;
         pixelClicado.style.backgroundColor = corEscolhida;
     }
-   
-    clearButton.addEventListener('click', limpaPixel);
+    window.alert("Ola! para pintar o sete insira o tamanho do quadro");    
+
     cores.addEventListener('click', selecionaCor);
     pixelBoard.addEventListener('click', colorePixel);
-   // tamanho =  botaoGeraQuadro.addEventListener('click', checaEntrada(boardSize))
-   // tamanhoDoBoard =  configuraBoard(tamanho);
-    //pixelBoard.innerHTML = " ";
-    //pixelBoard.innerHTML = tamanhoDoBoard;
+    botaoGeraQuadro.addEventListener('click', checaEntrada())
+    //console.log(tamanho);
+    //checaEntrada(tamanho);
+   //tamanhoDoBoard =  configuraBoard(tamanho);
+   // pixelBoard.innerHTML = " ";
