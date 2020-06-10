886a29b00a080da2bd7fe27d1feadebbff0737c8
Elisa <elisatcamillo@gmail.com>
2020-06-09 22:05:55 -0300

    changes i had forgotten to commit

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
-let clear_button = document.getElementById('clear-board');
-let all_pixels = document.getElementsByClassName('pixel')
+const color1 = document.getElementById('color1');
+const color2 = document.getElementById('color2');
+const color3 = document.getElementById('color3');
+const color4 = document.getElementById('color4');
+const clearButton = document.getElementById('clear-board');
+const allPixels = document.getElementsByClassName('pixel');
+const allColors = document.getElementsByClassName('color');
+let selectedColor = 'black';
 
-clear_button.addEventListener('click', clear);
+// adding event listener to all pixels
+for (let i = 0; i < allPixels.length; i+=1) {
+  allPixels[i].addEventListener('click', changeColor);
+}
 
-function clear(){
-  for (let i = 0; i < all_pixels.length; i++){
-    all_pixels[i].style.backgroundColor = 'white'
+// changing backgound color of selected pixel
+function changeColor(e) {
+  e.target.style.backgroundColor = selectedColor;
+}
+
+// remove class from all colors then apply to selected
+function changeClass(a) {
+  for (let i = 0; i < allColors.length; i+=1) {
+    allColors[i].classList.remove('selected');
+  }
+  a.classList.add('selected');
+}
+
+// clicking on a color calls for changeClass and changes value of selectedColor
+function changeColor1() {
+  selectedColor = 'black';
+  changeClass(color1);
+}
+
+function changeColor2() {
+  selectedColor = 'rgb(254, 200, 216)';
+  changeClass(color2);
+}
+
+function changeColor3() {
+  selectedColor = 'rgb(255, 223, 211)';
+  changeClass(color3);
+}
+
+function changeColor4() {
+  selectedColor = 'rgb(224, 187, 228)';
+  changeClass(color4);
+}
+
+// listening to clicks on palette and calling function to change selectedColor
+color1.addEventListener('click', changeColor1);
+color2.addEventListener('click', changeColor2);
+color3.addEventListener('click', changeColor3);
+color4.addEventListener('click', changeColor4);
+
+// clearing all pixels
+function clear() {
+  for (let i = 0; i < allPixels.length; i+=1) {
+    allPixels[i].style.backgroundColor = 'white';
   }
 }
+
+clearButton.addEventListener('click', clear);
