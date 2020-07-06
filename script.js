8693576b632c175d5f4eed58c9edfd95922224b2
Isabela Rosa <isabelarosa954@gmail.com>
2020-07-05 23:18:27 -0300

    corrigindo alguns erros do avaliador

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // testes:
-const divMae = document.createElement("div");
-document.getElementsByTagName("body")[0].appendChild(divMae);
-divMae.className = "divMae";
+const divMae = document.createElement('div');
+document.getElementsByTagName('body')[0].appendChild(divMae);
+divMae.className = 'divMae';
 
 const divFilho = document.createElement("div");
-document.querySelector("div.divMae").appendChild(divFilho);
+document.querySelector('div.divMae').appendChild(divFilho);
