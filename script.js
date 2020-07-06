87d9b68b25614af2bc6ef90c8167e1a7bc84c475
Isabela Rosa <isabelarosa954@gmail.com>
2020-07-06 01:00:17 -0300

    implementando função que limpa os pixels e tentando corrigir problema com as classes no css

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// testes:
-const divMae = document.createElement('div');
-document.getElementsByTagName('body')[0].appendChild(divMae);
-divMae.className = 'divMae';
-
-const divFilho = document.createElement('div');
-document.querySelector('div.divMae').appendChild(divFilho);
+// criando a função que limpará todos os pixels:
+let botaoLimpar = document.getElementById('clear-board');
+let todosPixels = document.getElementsByClassName('pixel');
+function limpaPixels () {
+  for (let i = 0; i < todosPixels.length; i += 1) {
+  todosPixels[i].style.backgroundColor = 'white';
+  }
+};
+botaoLimpar.addEventListener('click', limpaPixels);
