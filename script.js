6a3d57fcd153ef33c6a7b9aa65a2c30377a82836
Isabela Rosa <isabelarosa954@gmail.com>
2020-07-15 01:40:01 -0300

    testando função que limpa pixels noovamente 2

--- script.js
+++ script.js
@@ -50,13 +50,13 @@ pixelBoard.addEventListener('click', colorePixels); */
 
 // criando a função que limpará todos os pixels:
 const botaoLimpar = document.getElementById('clear-board');
-const todosPixels = document.getElementsByClassName('pixel');
+/*const todosPixels = document.getElementsByClassName('pixel');
 function limpaPixels() {
   for (let i = 0; i < todosPixels.length; i += 1) {
     todosPixels[i].style.backgroundColor = 'rgb(255, 255, 255)';
   }
-}
-botaoLimpar.addEventListener('click', limpaPixels);
+} */
+botaoLimpar.addEventListener('click', refazQuadroPixels);
 
 function geraRgb() {
   const r = Math.ceil(255 * Math.random());
@@ -100,4 +100,10 @@ botaoQuadroPersonalizado.addEventListener('click', quadroPersonalizado);
 window.onload = function() {
   criaQuadroPixels(quant);
   geraPaleta();
+}
+
+function colorePixel(evento) {
+  const click = evento.target;
+  const corClicada = document.querySelector('.selected').style.backgroundColor;
+  click.style.backgroundColor = corClicada;
 }
